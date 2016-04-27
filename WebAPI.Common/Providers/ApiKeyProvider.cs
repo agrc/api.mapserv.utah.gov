3992ccb459d84b0fe45c0f9a746cf5c66ce99efa
steveoh <sgourley@utah.gov>
2016-04-27 14:17:51 -0600

    create api key provider. extracts the api key from a request

---
+++ WebAPI.Common/Providers/ApiKeyProvider.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebAPI.Common.Providers
+{
+    public class ApiKeyProvider
+    {
+        public virtual async Task<string> GetApiFromRequestAsync(HttpRequestMessage request)
+        {
+            try
+            {
+                var key = HttpUtility.ParseQueryString(request.RequestUri.Query).Get("apikey");
+
+                if (!string.IsNullOrEmpty(key))
+                {
+                    return key;
+                }
+            }
+            catch
+            {
+            }
+
+            try
+            {
+                var formData = await request.Content.ReadAsFormDataAsync();
+
+                if (formData.AllKeys.Contains("apikey") && !string.IsNullOrEmpty(formData.Get("apikey")))
+                {
+                    return formData.Get("apikey");
+                }
+            }
+            catch
+            {
+            }
+
+            if (request.RequestUri.AbsolutePath.ToLower() == "/api/v1/geocode/ago/agrc-ago/findaddresscandidates")
+            {
+                return "agrc-ago";
+            }
+
+            return null;
+        }
+    }
+}
