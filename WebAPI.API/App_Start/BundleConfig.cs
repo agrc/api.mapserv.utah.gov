3f2371145c8946a3fef9c9aee3f252c6e7f18011
steveoh <sgourley@utah.gov>
2016-11-07 20:28:45 -0700

    update cdn location

--- WebAPI.API/App_Start/BundleConfig.cs
+++ WebAPI.API/App_Start/BundleConfig.cs
@@ -12,7 +12,7 @@ namespace WebAPI.API
                 "~/Scripts/jquery-1.10.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap",
-                                         "//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js").
+                                         "//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js").
                             Include(
                                 "~/Scripts/bootstrap.js",
                                 "~/Scripts/scrollspy.js"));
