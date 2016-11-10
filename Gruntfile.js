db0258d6fd0b82fbb3de437731726fc06394fa76
steveoh <sgourley@utah.gov>
2016-11-09 17:42:56 -0700

    remove scrollspy from grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,6 @@ module.exports = function (grunt) {
             webapi: {
                 files: {
                     'WebAPI.API/Scripts/site.min.js': 'WebAPI.API/Scripts/site.js',
-                    'WebAPI.API/Scripts/scrollspy.min.js': 'WebAPI.API/Scripts/scrollspy.js',
                     'WebAPI.API/Scripts/modernizr.min.js': 'WebAPI.API/Scripts/modernizr-2.6.1.js',
                     'WebAPI.API/Scripts/jquery.min.js': 'WebAPI.API/Scripts/jquery-1.10.1.js'
                 }
