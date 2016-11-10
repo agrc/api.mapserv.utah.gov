8b190998975fc340054ee176686cfa9043f6491b
steveoh <sgourley@utah.gov>
2016-11-09 17:13:09 -0700

    Release v1.1.0
    closes #18

--- WebAPI.Dashboard/Properties/AssemblyInfo.cs
+++ WebAPI.Dashboard/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Utah Automated Geographic Reference Center")]
 [assembly: AssemblyProduct("WebAPI.API")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.1111.1")]
-[assembly: AssemblyFileVersion("1.0.1111.1")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
