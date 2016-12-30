25e7364a907480459a84878592f58e9d63eadfb0
steveoh <sgourley@utah.gov>
2016-12-29 19:21:01 -0700

    add canary controller endpoint

---
+++ WebAPI.API/Controllers/CanaryController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using WebAPI.API.Commands.Info;
+using WebAPI.Common.Executors;
+
+namespace WebAPI.API.Controllers
+{
+    public class CanaryController : Controller
+    {
+        [HttpGet]
+        public JsonResult Index()
+        {
+            var result = CommandExecutor.ExecuteCommand(new FlyCanaryCommand());
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
