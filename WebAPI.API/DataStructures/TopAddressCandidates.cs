f343edf6822de6ac77480944ff77e7428a15b4fe
steveoh <sgourley@utah.gov>
2017-04-24 18:37:18 -0600

    update structure to allow score difference calc

--- WebAPI.API/DataStructures/TopAddressCandidates.cs
+++ WebAPI.API/DataStructures/TopAddressCandidates.cs
@@ -7,6 +7,18 @@ namespace WebAPI.API.DataStructures
     public class TopAddressCandidates : TopNList<Candidate>
     {
         public TopAddressCandidates(int size, IComparer<Candidate> comparer)
-            : base(size + 1, comparer) {}
+            : base(ChooseSize(size), comparer) {}
+
+        private static int ChooseSize(int size)
+        {
+            // allow score difference calculation for when no suggestion is selected
+            if (size == 0)
+            {
+                return 2;
+            }
+
+            // otherwise suggestion size + 1 since one is the top candidate
+            return size + 1;
+        }
     }
 }
