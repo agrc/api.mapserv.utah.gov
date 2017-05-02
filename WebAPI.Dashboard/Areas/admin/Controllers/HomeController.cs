86c3d33b32ec6d737fabcd3840017e78ea89b273
steveoh <sgourley@utah.gov>
2017-05-02 13:30:26 -0600

    remove old analytics stuff

--- WebAPI.Dashboard/Areas/admin/Controllers/HomeController.cs
+++ WebAPI.Dashboard/Areas/admin/Controllers/HomeController.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
-using AutoMapper;
 using Raven.Client;
-using WebAPI.Common.Extensions;
-using WebAPI.Common.Indexes;
 using WebAPI.Common.Models.Raven.Admin;
 using WebAPI.Dashboard.Controllers;
-using WebAPI.Dashboard.Models.ViewModels.Usage;
 
 namespace WebAPI.Dashboard.Areas.admin.Controllers
 {
@@ -33,36 +27,8 @@ namespace WebAPI.Dashboard.Areas.admin.Controllers
                     });
             }
 
-            var stats = Session.Query<StatsPerService.Stats, StatsPerService>()
-                               .Select(x => x).ToList();
 
-            var statsModel = Mapper.Map<List<StatsPerService.Stats>, List<ServiceStatsViewModel>>(stats);
-
-            var then = DateTime.Parse("2/24/2013").Ticks;
-            var lineChart = Session.Query<RequestsPerDay.Stats, RequestsPerDay>()
-                                   .Where(x => x.Date > then)
-                                   .OrderByDescending(x=>x.Date)
-                                   .ToList();
-
-            var line = TransformToD3JsLine(lineChart);
-
-            var model = new
-                {
-                    stats = statsModel,
-                    lineChart = line
-                };
-
-            return View("Index", model.ToExpando());
-        }
-
-        private static string TransformToD3JsLine(IEnumerable<RequestsPerDay.Stats> statsModel)
-        {
-            return string.Join(",", statsModel.
-                Select(x => string.Format("[{0}]", string.Join(",", new[]
-                    {
-                       string.Format("'{0}'", new DateTime(x.Date).ToShortDateString()), 
-                        x.Requests.ToString()
-                    }))));
+            return View("Index");
         }
     }
 }
