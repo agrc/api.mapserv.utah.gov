78ca6a047c0d3a9604689e45c4dda2ba05e79a17
steveoh <sgourley@utah.gov>
2017-05-10 10:54:45 -0600

    log specific api requests
    refs #36

--- WebAPI.API/Handlers/Delegating/ApiLoggingHandler.cs
+++ WebAPI.API/Handlers/Delegating/ApiLoggingHandler.cs
@@ -26,7 +26,7 @@ namespace WebAPI.API.Handlers.Delegating
         public ApiKeyProvider ApiKeyProvider { get; set; }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
-                                                               CancellationToken cancellationToken)
+            CancellationToken cancellationToken)
         {
             if (!request.Properties.Any())
             {
@@ -37,10 +37,10 @@ namespace WebAPI.API.Handlers.Delegating
             var apikey = await ApiKeyProvider.GetApiFromRequestAsync(request);
 
             return await base.SendAsync(request, cancellationToken).ContinueWith(
-                response => LogResponse(apikey, response), cancellationToken);
+                response => LogResponse(apikey, response, request), cancellationToken);
         }
 
-        private HttpResponseMessage LogResponse(string apikey, Task<HttpResponseMessage> response)
+        private HttpResponseMessage LogResponse(string apikey, Task<HttpResponseMessage> response, HttpRequestMessage request)
         {
             if (string.IsNullOrEmpty(apikey))
             {
@@ -52,7 +52,40 @@ namespace WebAPI.API.Handlers.Delegating
             db.StringIncrement(apikey, flags: CommandFlags.FireAndForget);
             db.StringSet(apikey + ":time", DateTime.UtcNow.Ticks, flags: CommandFlags.FireAndForget);
 
+            var routeData = RouteDataProvider.GetRouteData(request);
+
+            if (string.IsNullOrEmpty(routeData.Controller) ||
+                string.IsNullOrEmpty(routeData.Action) ||
+                string.IsNullOrEmpty(apikey))
+            {
+                return response.Result;
+            }
+
+            switch (routeData.Controller.ToUpper())
+            {
+                case "GEOCODE":
+                {
+                    LogRequest("geocode", apikey, db);
+                    break;
+                }
+                case "INFO":
+                {
+                    LogRequest("info", apikey, db);
+                    break;
+                }
+                case "SEARCH":
+                {
+                    LogRequest("search", apikey, db);
+                    break;
+                }
+            }
+
             return response.Result;
+        }
+
+        private static void LogRequest(string type, string key, IDatabase db)
+        {
+            db.StringIncrement(string.Format("{0}:{1}", key, type), flags: CommandFlags.FireAndForget);
         }
     }
 }
