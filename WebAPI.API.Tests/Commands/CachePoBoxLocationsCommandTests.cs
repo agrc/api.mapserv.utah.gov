59f73bb89c3b6dc16069de91a184d91967742c0a
steveoh <sgourley@utah.gov>
2017-06-28 15:25:44 -0600

    convert update-cache to c#

---
+++ WebAPI.API.Tests/Commands/CachePoBoxLocationsCommandTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using WebAPI.API.Commands.Sgid;
+
+namespace WebAPI.API.Tests.Commands
+{
+    [TestFixture]
+    public class CachePoBoxLocationsCommandTests
+    {
+        [Test]
+        public void CanOpenFc()
+        {
+            var command = new CachePoBoxLocationsCommand();
+            command.Run();
+
+            Assert.That(command.Result, Is.Not.Empty);
+        }
+    }
+}
---
+++ WebAPI.API/Commands/ArcGIS/GetWorkspaceCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ESRI.ArcGIS;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using WebAPI.Common.Abstractions;
+
+namespace WebAPI.API.Commands.ArcGIS
+{
+    public class GetWorkspaceCommand : Command<IWorkspace>
+    {
+        private readonly string _connectionString;
+
+        public GetWorkspaceCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public override string ToString()
+        {
+            return "GetWorkspaceCommand";
+        }
+
+        protected override void Execute()
+        {
+            RuntimeManager.Bind(ProductCode.Server);
+
+            var init = new AoInitialize();
+            init.Initialize(esriLicenseProductCode.esriLicenseProductCodeBasic);
+
+            var factoryType = Type.GetTypeFromProgID("esriDataSourcesGDB.SdeWorkspaceFactory");
+            var workspaceFactory2 = (IWorkspaceFactory2)Activator.CreateInstance(factoryType);
+
+            Result = workspaceFactory2.OpenFromFile(_connectionString, 0);
+        }
+    }
+}
