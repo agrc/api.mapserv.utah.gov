435043499d28b8244c26b7ecdbe40ca747377dc3
steveoh <sgourley@utah.gov>
2017-06-28 15:25:44 -0600

    convert update-cache to c#
    closes #54

---
+++ WebAPI.API.Tests/Commands/GetCacheDateaFromDriveTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using WebAPI.API.Commands.Drive;
+
+namespace WebAPI.API.Tests.Commands
+{
+    [TestFixture]
+    public class GetCacheDateaFromDriveTests
+    {
+        [Test]
+        public void CanAuthenticate()
+        {
+            var command = new GetCachedDataFromDriveCommand();
+            command.Run();
+
+            Assert.That(command.Result, Is.Not.Null);
+            Assert.That(command.Result.ZipCodesGrids, Is.Not.Empty);
+            Assert.That(command.Result.PlaceGrids, Is.Not.Empty);
+            Assert.That(command.Result.UspsDeliveryPoints, Is.Not.Empty);
+            Assert.That(command.Result.PoBoxExclusions, Is.Not.Empty);
+        }
+    }
+}
