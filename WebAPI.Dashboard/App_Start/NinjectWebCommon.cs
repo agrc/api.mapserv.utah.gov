f0d01e1f0e2b3bf0ea56d87a444808c69bd2ca7f
steveoh <sgourley@utah.gov>
2018-02-08 17:44:33 -0700

    updated ninject, build working

--- WebAPI.Dashboard/App_Start/NinjectWebCommon.cs
+++ WebAPI.Dashboard/App_Start/NinjectWebCommon.cs
@@ -3,6 +3,7 @@ using System.Web;
 using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 using Ninject;
 using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
 using WebAPI.Common.Ninject.Modules;
 using WebAPI.Dashboard;
 using WebAPI.Dashboard.Ninject.Modules;
