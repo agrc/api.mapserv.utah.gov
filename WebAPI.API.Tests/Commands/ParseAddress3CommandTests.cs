7659ef79372ec638155d7ce0b69bf612482a3495
steveoh <sgourley@utah.gov>
2018-02-13 13:11:26 -0700

    start of an idea

---
+++ WebAPI.API.Tests/Commands/ParseAddress3CommandTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+using WebAPI.API.Commands.Address;
+
+namespace WebAPI.API.Tests.Commands
+{
+    [TestFixture]
+    public class ParseAddress3CommandTests
+    {
+        [TestCase("123 north st.")]
+        [TestCase("123 north-west st")]
+        [TestCase("1:23 north-west st")]
+        public void RemoveExtraniousCharacters(string street)
+        {
+            var command = new ParseAddress3Command(street);
+
+            var result = command.RemoveExtraniousCharacters(street);
+
+            Console.WriteLine(result);
+            Assert.That(result.Contains("."), Is.False);
+            Assert.That(result.Contains("-"), Is.False);
+            Assert.That(result.Contains(":"), Is.False);
+        }
+
+        [TestFixture]
+        public class GetStreetParts
+        {
+            [TestCase("123 north st", 3)]
+            [TestCase("123 northwest st", 3)]
+            public void ReturnsCorrectCount(string street, int parts)
+            {
+                var command = new ParseAddress3Command(street);
+
+                var result = command.CreateStreetParts(street);
+
+                Assert.That(result.Count(), Is.EqualTo(parts));
+            }
+
+            [Test]
+            public void CreatesLinkedList()
+            {
+                const string street = "123 north st";
+                var command = new ParseAddress3Command(street);
+
+                var result = command.CreateStreetParts(street).ToArray();
+
+                var first = result.Single(x => x.Index == 0);
+                var second = result.Single(x => x.Index == 1);
+                var third = result.Single(x => x.Index == 2);
+
+                Assert.That(first.IsFirst, Is.True);
+                Assert.That(first.IsLast, Is.False);
+                Assert.That(first.Right, Is.EqualTo(result[1]));
+                Assert.That(first.Left, Is.Null);
+                Assert.That(first.GetValue(), Is.EqualTo("123"));
+
+                Assert.That(second.IsFirst, Is.False);
+                Assert.That(second.IsLast, Is.False);
+                Assert.That(second.Right, Is.EqualTo(result[2]));
+                Assert.That(second.Left, Is.EqualTo(result[0]));
+                Assert.That(second.GetValue(), Is.EqualTo("north"));
+
+                Assert.That(third.IsFirst, Is.False);
+                Assert.That(third.IsLast, Is.True);
+                Assert.That(third.Right, Is.Null);
+                Assert.That(third.Left, Is.EqualTo(result[1]));
+                Assert.That(third.GetValue(), Is.EqualTo("st"));
+            }
+        }
+    }
+}
---
+++ WebAPI.API/Commands/Address/ParseAddress3Command.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using WebAPI.Common.Abstractions;
+using WebAPI.Domain.Addresses;
+
+namespace WebAPI.API.Commands.Address
+{
+    public class ParseAddress3Command : Command<CleansedAddress>
+    {
+        private readonly string _original;
+        private readonly string _street;
+
+        public ParseAddress3Command(string street)
+        {
+            _street = _original = street;
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Execute()
+        {
+            var street = RemoveExtraniousCharacters(_street);
+            var parts = CreateStreetParts(street);
+        }
+
+        public string RemoveExtraniousCharacters(string street)
+        {
+            // TODO: keep # for unit numbers?
+            // TODO: do street names contain hyphens?
+            var alphaNumeric = new Regex("[^0-9|a-z|\\s]");
+
+            return alphaNumeric.Replace(_street, "");
+        }
+
+        public IEnumerable<StreetPart> CreateStreetParts(string street)
+        {
+            // TODO: create linked list so there is a first prop to get the starting point
+
+            var parts = street.Split(' ');
+            var streetParts = parts.Select((x, index) => new StreetPart(x, index));
+
+            var partHash = streetParts.ToDictionary(x => x.Index, y => y);
+            var length = partHash.Keys.Count - 1;
+
+            foreach (var pair in partHash)
+            {
+                var index = pair.Key;
+                var part = pair.Value;
+
+                if (index == 0 && index + 1 <= length)
+                {
+                    part.Link(null, partHash[index + 1]);
+                    continue;
+                }
+
+                if (index == length)
+                {
+                    part.Link(partHash[index - 1], null);
+                    continue;
+                }
+
+                part.Link(partHash[index - 1], partHash[index + 1]);
+            }
+
+            return partHash.Values.ToList();
+        }
+
+        public class StreetPart
+        {
+            private readonly string _value;
+
+            public StreetPart(string value, int index)
+            {
+                Index = index;
+                _value = value;
+
+                // TODO: does contain multiple parts
+                // eg: 123west 456south should be split from 2 to 4 parts
+
+                // TODO: determine value type
+                // eg: number, direction, street type, unit, po box, highway
+                // maybe create an enum of types?
+                IsNumber = new Regex("0-9+").IsMatch(value);
+                IsDirection = App.RegularExpressions["direction"].IsMatch(_value);
+                IsStreetType = App.RegularExpressions["streetType"].IsMatch(_value);
+                IsHighway = App.RegularExpressions["highway"].IsMatch(_value);
+                IsOrdinal = App.RegularExpressions["ordinal"].IsMatch(_value);
+
+                // TODO: based on type replace with correction or have other code do this?
+            }
+
+            public bool IsNumber { get; }
+
+            public bool IsDirection { get; }
+
+            public bool IsStreetType { get; }
+
+            public bool IsHighway { get; }
+
+            public bool IsOrdinal { get; }
+
+            public int Index { get; }
+
+            public StreetPart Left { get; private set; }
+
+            public StreetPart Right { get; private set; }
+
+            public bool IsFirst => Left == null;
+
+            public bool IsLast => Right == null;
+
+            public void Link(StreetPart left, StreetPart right)
+            {
+                Left = left;
+                Right = right;
+            }
+
+            public object GetValue()
+            {
+                return _value;
+            }
+        }
+    }
+}
