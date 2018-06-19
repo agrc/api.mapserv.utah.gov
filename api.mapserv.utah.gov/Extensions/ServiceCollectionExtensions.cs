b131de0c3a5650941de04547abe18274a442198c
steveoh <sgourley@utah.gov>
2018-06-18 18:59:05 -0600

    move service collection items into extension methods

---
+++ api.mapserv.utah.gov/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using api.mapserv.utah.gov.Cache;
+using api.mapserv.utah.gov.Commands;
+using api.mapserv.utah.gov.Filters;
+using api.mapserv.utah.gov.Models.SecretOptions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace api.mapserv.utah.gov.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void UseOptions(this IServiceCollection services, IConfiguration config){
+            services.Configure<GisServerConfiguration>(config);
+            services.Configure<GeometryServiceConfiguration>(config);
+            services.Configure<DbConfiguration>(config);
+        }
+
+        public static void UseDi(this IServiceCollection services)
+        {
+            services.AddHttpClient("default", client =>
+            {
+                //var httpClientHandler = new HttpClientHandler();
+                //if (httpClientHandler.SupportsAutomaticDecompression)
+                //{
+                //    httpClientHandler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                //}
+                client.Timeout = new TimeSpan(0, 0, 15);
+            });
+
+            services.AddSingleton<IAbbreviations, Abbreviations>();
+            services.AddSingleton<IRegexCache, RegexCache>();
+            services.AddSingleton<IGoogleDriveCache, GoogleDriveCache>();
+            services.AddSingleton<IBrowserKeyProvider, AuthorizeApiKeyFromRequest.BrowserKeyProvider>();
+            services.AddSingleton<IServerIpProvider, AuthorizeApiKeyFromRequest.ServerIpProvider>();
+            services.AddSingleton<AuthorizeApiKeyFromRequest, AuthorizeApiKeyFromRequest>();
+
+            services.AddTransient<ParseAddressCommand, ParseAddressCommand>();
+            services.AddTransient<ParseZoneCommand, ParseZoneCommand>();
+            services.AddTransient<GetLocatorsForAddressCommand, GetLocatorsForAddressCommand>();
+            services.AddTransient<LocatePoBoxCommand, LocatePoBoxCommand>();
+            services.AddTransient<UspsDeliveryPointCommand, UspsDeliveryPointCommand>();
+            services.AddTransient<ReprojectPointsCommand, ReprojectPointsCommand>();
+        }
+    }
+}
