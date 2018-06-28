e6a4b48eb10f161c7a050e1f751b5f38c09467ab
steveoh <sgourley@utah.gov>
2018-06-28 17:32:32 -0600

    feat: show email unconfirmed dialog

--- developer.mapserv.utah.gov/Areas/Secure/Controllers/HomeController.cs
+++ developer.mapserv.utah.gov/Areas/Secure/Controllers/HomeController.cs
@@ -64,6 +64,26 @@ namespace developer.mapserv.utah.gov.Areas.Secure.Controllers
                 id = Convert.ToInt32(idString);
             }
 
+            var confirmed = await Connection.QueryFirstOrDefaultAsync<bool>("SELECT email_confirmed from public.accounts where id = @id",
+                                                                       new
+                                                                       {
+                                                                           id
+                                                                       });
+
+            if (!confirmed)
+            {
+                var profileLink = Url.RouteUrl(new
+                {
+                    action = "index",
+                    controller = "profile",
+                    area = "secure"
+                });
+
+                ViewData["Error"] = "Your email has not been confirmed. While you <em>can</em> create API keys, they will <strong>not</strong> authorize access. " +
+                    $"Browse to your <a href='{profileLink}'>profile</a> and click the confirm button next to your email and " +
+                     "follow the instructions in your inbox.";
+            }
+
             var quota = await Connection.QueryFirstOrDefaultAsync<KeyQuotaDTO>("SELECT keys_used as keysused, keys_allowed as keysallowed FROM public.accounts WHERE id = @id",
                                                                  new
                                                                  {
