6b85dd4601f6a23261dcf6382079273d7923a36d
steveoh <sgourley@utah.gov>
2018-08-07 19:31:15 -0600

    feat: add serilog for asp logs

--- api.mapserv.utah.gov/Program.cs
+++ api.mapserv.utah.gov/Program.cs
@@ -1,26 +1,52 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using api.mapserv.utah.gov.Cache;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
 
 namespace api.mapserv.utah.gov
 {
     public static class Program
     {
-        public static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
 
-            var lookupCache = host.Services.GetService(typeof(ILookupCache)) as ILookupCache;
-            await lookupCache.InitializeAsync();
+            try
+            {
+                Log.Information("Starting web host");
 
-            await host.RunAsync();
+                var host = CreateWebHostBuilder(args).Build();
+
+                var lookupCache = host.Services.GetService(typeof(ILookupCache)) as ILookupCache;
+                await lookupCache.InitializeAsync();
+
+                await host.RunAsync();
+
+                return 0;
+            } 
+            catch (Exception ex) {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
-                          .UseStartup<Startup>();
+                          .UseStartup<Startup>()
+                          .UseSerilog();
         }
     }
 }
