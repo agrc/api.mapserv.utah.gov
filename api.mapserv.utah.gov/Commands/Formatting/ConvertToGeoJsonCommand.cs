bb253af11524b21b0b5d7f836e4d8c279f1ca99c
steveoh <sgourley@utah.gov>
2018-08-09 11:33:41 -0600

    fix: generate equal output json formatting for custom formats

--- api.mapserv.utah.gov/Commands/Formatting/ConvertToGeoJsonCommand.cs
+++ api.mapserv.utah.gov/Commands/Formatting/ConvertToGeoJsonCommand.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using api.mapserv.utah.gov.Models;
 using GeoJSON.Net.Feature;
 using GeoJSON.Net.Geometry;
+using Newtonsoft.Json.Linq;
 using Point = GeoJSON.Net.Geometry.Point;
 
 namespace api.mapserv.utah.gov.Commands.Formatting
@@ -30,7 +31,9 @@ namespace api.mapserv.utah.gov.Commands.Formatting
             if (result?.Location != null)
             {
                 geometry = new Point(new Position(result.Location.Y, result.Location.X));
-                attributes = GetProperties(result);
+
+                var jobj = JObject.FromObject(Container.Result);
+                attributes = jobj.ToObject<Dictionary<string, object>>();
             }
 
             if (geometry == null && attributes == null)
@@ -54,16 +57,6 @@ namespace api.mapserv.utah.gov.Commands.Formatting
             };
 
             Result = responseContainer;
-        }
-
-        public static Dictionary<string, object> GetProperties<T>(T obj)
-        {
-            var properties = typeof(T).GetProperties();
-
-            var dictionary = properties.ToDictionary(prop => prop.Name, prop => prop.GetValue(obj, null));
-
-            return dictionary.Where(x => x.Value != null)
-                             .ToDictionary(x => x.Key, x => x.Value);
         }
     }
 }
