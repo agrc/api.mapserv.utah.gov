6d586a1236582027a7611d4cdb271de660bc2846
steveoh <sgourley@utah.gov>
2018-08-09 17:26:21 -0600

    feat: reverse address geocoding

---
+++ api.mapserv.utah.gov/Models/ArcGis/RestErrorable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace api.mapserv.utah.gov.Models.ArcGis
+{
+    public abstract class RestErrorable
+    {
+        public virtual RestEndpointError Error { get; set; }
+
+        public virtual bool IsSuccessful
+        {
+            get { return Error == null; }
+        }
+    }
+
+    public class RestEndpointError
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public IEnumerable<object> Details { get; set; }
+    }
+}
