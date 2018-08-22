c4ca633c88b0c60db78a5a4caeb3a08ccf905b63
steveoh <sgourley@utah.gov>
2018-08-22 12:05:22 -0600

    test: 100% coverage

---
+++ tests/api.tests/Models/Configuration/GisServerConfigurationTests.cs
@@ -0,0 +1,37 @@
+using api.mapserv.utah.gov.Models.Configuration;
+using Shouldly;
+using Xunit;
+
+namespace api.tests.Models.Configuration {
+    public class GisServerConfigurationTests {
+        [Fact]
+        public void Should_format_url_with_non_standard_port()
+        {
+            var config = new GisServerConfiguration {
+                Host = "host",
+                Port = "1",
+                Protocol = "protocol"
+            };
+
+            config.ToString().ShouldBe("protocol://host:1");
+        }
+
+        [Fact]
+        public void Should_omit_standard_port() {
+            var config = new GisServerConfiguration {
+                Host = "host",
+                Port = "80",
+                Protocol = "protocol"
+            };
+
+            config.ToString().ShouldBe("protocol://host");
+        }
+
+        [Fact]
+        public void Should_set_good_default() {
+            var config = new GisServerConfiguration();;
+
+            config.ToString().ShouldBe("http://localhost");
+        }
+    }
+}
