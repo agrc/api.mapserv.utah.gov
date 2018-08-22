4e12f5727dcd5c231f23c2f3a6a2d121bd4c44a7
steveoh <sgourley@utah.gov>
2018-08-22 15:32:55 -0600

    test: delivery point tests

---
+++ tests/api.tests/Features/Geocoding/DeliveryPointTests.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using api.mapserv.utah.gov.Cache;
+using api.mapserv.utah.gov.Features.Geocoding;
+using api.mapserv.utah.gov.Models;
+using api.mapserv.utah.gov.Models.Constants;
+using api.mapserv.utah.gov.Models.Linkables;
+using api.mapserv.utah.gov.Models.RequestOptions;
+using MediatR;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace api.tests.Features.Geocoding {
+    public class DeliveryPointTests {
+        private readonly IDictionary<string, List<GridLinkable>> _deliveryPoints = new Dictionary<string, List<GridLinkable>>(1);
+        private readonly UspsDeliveryPointLocation.Handler _handler;
+
+        public DeliveryPointTests() {
+            _deliveryPoints.Add("84114", new GridLinkable[] { new UspsDeliveryPointLink(84114, "grid", "place", 1, 1) }.ToList());
+
+            var mockCache = new Mock<ILookupCache>();
+            mockCache.Setup(x => x.UspsDeliveryPoints).Returns(_deliveryPoints);
+
+            _handler = new UspsDeliveryPointLocation.Handler(mockCache.Object, new Mock<IMediator>().Object);
+        }
+
+        [Fact]
+        public async Task Should_return_null_if_zip_not_found() {
+            var parsedAddress = new CleansedAddress("inputAddress", 1, 0, 0, Direction.North, "street", StreetType.Alley, Direction.South, 0, -1, false, false);
+            var address = new GeocodeAddress(parsedAddress);
+
+            var geocodeOptions = new GeocodingOptions {
+                PoBox = true,
+            };
+
+            var request = new UspsDeliveryPointLocation.Command(address, geocodeOptions);
+            var result = await _handler.Handle(request, new CancellationToken());
+
+            result.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Should_return_null_for_address_without_zip_code() {
+            var parsedAddress = new CleansedAddress("inputAddress", 1, 0, 0, Direction.North, "street", StreetType.Alley, Direction.South, 0, new Nullable<int>(), false, false);
+            var address = new GeocodeAddress(parsedAddress);
+
+            var geocodeOptions = new GeocodingOptions {
+                PoBox = true
+            };
+
+            var request = new UspsDeliveryPointLocation.Command(address, geocodeOptions);
+            var result = await _handler.Handle(request, new CancellationToken());
+
+            result.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Should_return_candidate_from_cache() {
+            var pobox = -1;
+            var zip = 84114;
+
+            var parsedAddress = new CleansedAddress("inputAddress", 1, 0, pobox, Direction.North, "street", StreetType.Alley, Direction.South, 0, zip, false, false);
+            var address = new GeocodeAddress(parsedAddress);
+            address.AddressGrids = new[] { new ZipGridLink(84114, "grid", 0) };
+
+            var geocodeOptions = new GeocodingOptions {
+                PoBox = true,
+                SpatialReference = 26912
+            };
+
+            var request = new UspsDeliveryPointLocation.Command(address, geocodeOptions);
+            var result = await _handler.Handle(request, new CancellationToken());
+
+            result.Score.ShouldBe(100);
+            result.Locator.ShouldBe("USPS Delivery Points");
+            result.Location.X.ShouldBe(1);
+            result.Location.Y.ShouldBe(1);
+            result.AddressGrid.ShouldBe("grid");
+        }
+    }
+}
