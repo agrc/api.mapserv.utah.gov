64bbcf866225edc9cf4625d51c78f983eb995aaf
steveoh <sgourley@utah.gov>
2018-09-04 16:31:50 -0600

    refactor: rename file

--- api.mapserv.utah.gov/Models/Configuration/GeometryServiceConfiguration.cs
+++ api.mapserv.utah.gov/Models/Configuration/GeometryServiceConfiguration.cs
@@ -1,5 +1,5 @@
 namespace api.mapserv.utah.gov.Models.Configuration {
-    public class GeometryServiceConfiguration : UrlConfigurationBase {
+    public class GeometryServiceConfiguration : GisServerConfigurationBase {
         // http://localhost:6080/arcgis/rest/services/Geometry/GeometryServer/project?f=json&amp;{0}
         private const string Template = "{Host}/{ServiceLocation}/project{0}";
         public string Path { get; set; } = "/arcgis/rest/services/Geometry/GeometryServer/";
