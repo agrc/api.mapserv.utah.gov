a6ea0dd26f88c7c2a34eb97a335cda3df6c67328
steveoh <sgourley@utah.gov>
2018-09-06 18:13:51 -0600

    feat: import geocoding samples

---
+++ samples/README.md
@@ -0,0 +1,21 @@
+# GeocodingSample
+
+How to geocode an address in various programming languages.
+
+## Languages implemented
+
+- [x] JavaScript
+- [x] Ruby
+- [x] Java
+- [x] Python
+- [x] PHP
+- [x] C#
+- [x] VB.NET
+- [ ] Your contributed language! 🎉
+
+## Contributing
+Please read the other language samples and write the functionally same application.
+
+- return the same data
+- log the same values
+- keep similar naming convention
---
+++ samples/VB/Program.vb
@@ -0,0 +1,14 @@
+﻿Module Program
+
+    Sub Main()
+        Dim g = new Geocoder("your api key here")
+        Dim location = g.Locate("123 South Main Street", "SLC", New Dictionary(Of String, Object) From {
+                                   {"score", 90},
+                                   {"spatialReference", 4326}
+                                   })
+
+        Console.WriteLine(location)
+        Console.ReadKey()
+    End Sub
+
+End Module
