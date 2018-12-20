509e4e8dc7c7505b2b3b4db4aab05980f4609d34
steveoh <sgourley@utah.gov>
2018-12-19 20:09:49 -0700

    feat: add performance logging behavior

---
+++ src/api/Infrastructure/PerformanceLogger.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Serilog;
+
+namespace api.mapserv.utah.gov.Infrastructure {
+    public class PerformanceLogger<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> {
+        private readonly Stopwatch _timer;
+        private readonly ILogger _log;
+
+        public PerformanceLogger(ILogger log) {
+            _timer = new Stopwatch();
+            _log = log;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next) {
+            _timer.Start();
+
+            var response = await next();
+
+            _timer.Stop();
+
+            if (_timer.ElapsedMilliseconds > 500) {
+                var name = typeof(TRequest).Name;
+
+                _log.Warning("Long Running Request: {Name} ({ElapsedMilliseconds} ms) {@Request}", name, _timer.ElapsedMilliseconds, request);
+            }
+
+            return response;
+        }
+    }
+}
