9a7b042b0a0a3037c86e5406043725c3e51e7a0e
steveoh <sgourley@utah.gov>
2018-12-19 19:42:33 -0700

    refactor: split api methods into their own controllers

--- src/api/Features/Geocoding/ApiReverseController.cs
+++ src/api/Features/Geocoding/ApiReverseController.cs
@@ -31,36 +31,13 @@ namespace api.mapserv.utah.gov.Features.Geocoding {
     [ApiVersion("2.0")]
     [Produces("application/json")]
     [ServiceFilter(typeof(AuthorizeApiKeyFromRequest))]
-    public class GeocodeAddressController : ControllerBase {
+    public class ApiReverseController : ControllerBase {
         private readonly ILogger _log;
         private readonly IMediator _mediator;
 
-        public GeocodeAddressController(IMediator mediator, ILogger log) {
+        public ApiReverseController(IMediator mediator, ILogger log) {
             _mediator = mediator;
             _log = log;
-        }
-
-        /// <summary>
-        ///     Finds the x, y location for an input address
-        /// </summary>
-        /// <remarks>Requires an API Key</remarks>
-        /// <response code="200">The address was geocoded successfully</response>
-        /// <response code="400">The input address was not well formed</response>
-        /// <response code="404">The input address was unable to be geocoded</response>
-        /// <response code="500">Something went terribly wrong</response>
-        /// <param name="street">A Utah street address. eg: 326 east south temple st. Intersections are separated by `and`</param>
-        /// <param name="zone">A Utah municipality name or 5 digit zip code</param>
-        /// <param name="options"></param>
-        [HttpGet, MapToApiVersion("2.0")]
-        [ApiConventionMethod(typeof(ApiConventions), nameof(ApiConventions.Default))]
-        [Route("api/v{version:apiVersion}/geocode/{street}/{zone}")]
-        public async Task<ActionResult<ApiResponseContainer<GeocodeAddressApiResponse>>> Geocode(string street, string zone, [FromQuery] GeocodingOptions options) {
-            _log.Debug("Geocoding {street}, {zone} with options: {options}", street, zone, options);
-
-            var geocodeAddressCommand = new GeocodeAddress.Command(street, zone, options);
-            var result = await _mediator.Send(geocodeAddressCommand);
-
-            return result;
         }
 
         /// <summary>
@@ -117,7 +94,7 @@ namespace api.mapserv.utah.gov.Features.Geocoding {
             }
 
             // TODO: would there ever be more than one?
-            var reverseGeocodeCommand = new ReverseGeocode.Command(locators.First());
+            var reverseGeocodeCommand = new ReverseGeocodeQuery.Command(locators.First());
 
             try {
                 var response = await _mediator.Send(reverseGeocodeCommand);
