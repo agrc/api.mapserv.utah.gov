627a2ed1a51537102e5d5b6216a88bf70e1a37b8
steveoh <sgourley@utah.gov>
2019-02-05 12:58:55 -0700

    chore: Soe Release v1.6.0

--- WebAPI.Search.Soe/Properties/AssemblyInfo.cs
+++ WebAPI.Search.Soe/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 using ESRI.ArcGIS.SOESupport;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
@@ -16,8 +16,8 @@ using ESRI.ArcGIS.SOESupport;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("D837482C-8E72-45A0-9CD3-D49F517533D3")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
 [assembly: AddInPackage("WebAPI.Search.Soe", "021ED22C-4D23-4A84-9CC3-A16D24BA1D5E",
     Author = "@steveoh",
     Company = "agrc",
