c27ddaa02dacfdd05459e3cc37443bcd1c04e763
steveoh <sgourley@utah.gov>
2019-09-02 21:01:14 -0600

    release(soe): v1.6.1

--- WebAPI.Search.Soe/Properties/AssemblyInfo.cs
+++ WebAPI.Search.Soe/Properties/AssemblyInfo.cs
@@ -16,13 +16,13 @@ using ESRI.ArcGIS.SOESupport;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("D837482C-8E72-45A0-9CD3-D49F517533D3")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
 [assembly: AddInPackage("WebAPI.Search.Soe", "021ED22C-4D23-4A84-9CC3-A16D24BA1D5E",
     Author = "@steveoh",
     Company = "agrc",
     Date = "12/10/2012 5:56:42 PM",
     Description = "assembly info description",
     TargetProduct = "Server",
-    TargetVersion = "10.2",
+    TargetVersion = "10.5",
     Version = "1.0")]
