f0a9874fbb72e453a0c941edc957dc54e0bac026
steveoh <sgourley@utah.gov>
2020-03-10 16:00:17 -0600

    refactor(explorer): create a button compontent

--- src/explorer/src/stories/Website.stories.js
+++ src/explorer/src/stories/Website.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Action from '../components/Action';
 import { SideNav, TopNav } from '../components/Navigation';
+import Button from '../components/Button';
 
 export default {
   title: 'Website Parts'
@@ -11,3 +12,9 @@ export const Headers = () => <Header></Header>;
 export const CallToAction = () => <Action></Action>;
 export const SideNavigation = () => <SideNav></SideNav>;
 export const TopNavigation = () => <TopNav></TopNav>;
+export const Buttons = () => (
+  <>
+    <Button>normal</Button>
+    <Button disabled>disabled</Button>
+  </>
+);
