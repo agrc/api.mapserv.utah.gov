86566acbee2da8989edd599fd8f0895ddcaeaa40
steveoh <sgourley@utah.gov>
2020-03-10 16:04:22 -0600

    refactor(explorer): move initial and default state to separate file

---
+++ src/explorer/src/components/Endpoint/Geocoding/StreetZone/meta.js
@@ -0,0 +1,61 @@
+const initialState = {
+  street: '',
+  zone: '',
+  spatialReference: '26912',
+  acceptScore: '70',
+  pobox: false,
+  locators: 'all',
+  format: 'default',
+  suggest: '0',
+  scoreDifference: false,
+  callback: ''
+};
+
+const defaultAttributes = {
+  street: {
+    placeholder: '123 south main street',
+    type: 'string',
+    required: true
+  },
+  zone: {
+    placeholder: 'SLC',
+    type: 'string',
+    required: true
+  },
+  sr: {
+    placeholder: 26912,
+    type: 'int',
+    required: false
+  },
+  acceptScore: {
+    placeholder: 70,
+    type: 'int',
+    required: false
+  },
+  pobox: {
+    type: 'boolean',
+    required: false
+  },
+  locators: {
+    placeholder: 'all',
+    type: 'string',
+    required: false
+  },
+  suggest: {
+    placeholder: 0,
+    type: 'int',
+    required: false
+  },
+  scoreDifference: {
+    placeholder: 0,
+    type: 'int',
+    required: false
+  },
+  callback: {
+    placeholder: 'callback',
+    type: 'string',
+    required: false
+  }
+};
+
+export { initialState, defaultAttributes };
