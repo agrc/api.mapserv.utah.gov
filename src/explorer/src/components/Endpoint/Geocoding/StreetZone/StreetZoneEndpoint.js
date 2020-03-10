a592918e87eacafa82aa65332812e47f6b5a5268
steveoh <sgourley@utah.gov>
2020-03-10 16:05:18 -0600

    feat(explorer): fetch api and sytax highlight response

--- src/explorer/src/components/Endpoint/Geocoding/StreetZone/StreetZoneEndpoint.js
+++ src/explorer/src/components/Endpoint/Geocoding/StreetZone/StreetZoneEndpoint.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Endpoint from '../../Endpoint';
 import StreetZoneApi from './StreetZoneApi';
 import StreetZoneDocs from './StreetZoneDocs';
 
 export default function StreetZone(props) {
+  const [url, setUrl] = useState('');
+
   return (
-    <Endpoint collapsed={props.collapsed} name="Street and Zone" description="Finding a spatial coordinate on the ground for an address">
-      <StreetZoneApi key="api"></StreetZoneApi>
+    <Endpoint url={url} collapsed={props.collapsed} name="Street and Zone" description="Finding a spatial coordinate on the ground for an address">
+      <StreetZoneApi setUrl={setUrl} key="api"></StreetZoneApi>
       <StreetZoneDocs key="docs"></StreetZoneDocs>
     </Endpoint>
   );
