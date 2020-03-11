69bd51f002d9b5cab7a137b6947c0e659f2298ed
steveoh <sgourley@utah.gov>
2020-03-11 14:13:28 -0600

    style(explorer): make doc/demo toggler more visible

--- src/explorer/src/components/Endpoint/EndpointDemoDocToggle.js
+++ src/explorer/src/components/Endpoint/EndpointDemoDocToggle.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const base = ['text-center', 'block', 'border', 'border-indigo-600', 'py-2', 'px-4', 'duration-150', 'transform', 'ease-in-out', 'focus:outline-none'];
+const base = [
+  'text-center',
+  'text-sm',
+  'tracking-wider',
+  'uppercase',
+  'block',
+  'border',
+  'border-indigo-600',
+  'py-1',
+  'px-4',
+  'duration-150',
+  'transform',
+  'ease-in-out',
+  'focus:outline-none',
+  'shadow'
+];
 
-const normalCss = ['hover:bg-indigo-500', 'text-indigo-600', 'hover:text-white'];
+const normalCss = ['bg-gray-100', 'hover:bg-indigo-500', 'text-indigo-600', 'hover:text-white'];
 const activeCss = ['bg-indigo-500', 'text-white'];
 
 export default function Toggle(props) {
@@ -14,17 +29,19 @@ export default function Toggle(props) {
   const normalDoc = classNames('rounded-tr-lg', 'border-l-none', base, normalCss);
 
   return (
-    <ul className="flex justify-center mt-2">
-      <li>
-        <button className={props.active === 'api' ? activeApi : normalApi} onClick={() => props.showApi(true)}>
-          API Demo
-        </button>
-      </li>
-      <li>
-        <button className={props.active === 'docs' ? activeDoc : normalDoc} onClick={() => props.showApi(false)}>
-          More Documentation
-        </button>
-      </li>
-    </ul>
+    <div className="bg-gray-200 shadow-inner border-b border-gray-300">
+      <ul className="flex justify-center p-3">
+        <li>
+          <button className={props.active === 'api' ? activeApi : normalApi} onClick={() => props.showApi(true)}>
+            demo
+          </button>
+        </li>
+        <li>
+          <button className={props.active === 'docs' ? activeDoc : normalDoc} onClick={() => props.showApi(false)}>
+            documentation
+          </button>
+        </li>
+      </ul>
+    </div>
   );
 }
