bdf2b3e7d777435dd7bfcdb89b23603fe4811b17
steveoh <sgourley@utah.gov>
2020-03-11 14:12:30 -0600

    refactor(explorer): let tailwind manage the css

--- src/explorer/src/components/Endpoint/EndpointSwitch.js
+++ src/explorer/src/components/Endpoint/EndpointSwitch.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import './EndpointSwitch.css';
+// import './EndpointSwitch.css';
 
 import Toggle from 'react-toggle';
 import { TypeLabel, RequiredLabel } from './EndpointLabel';
 
 export default function Field(props) {
   return (
-    <div className="flex flex-wrap pt-3 md:justify-around md:flex-no-wrap">
+    <div className="flex flex-wrap w-full pt-3 md:flex-no-wrap items-center">
       <label className="self-center mx-2 md:ml-4 pb-2 md:pb-2 text-sm leading-5 font-medium text-gray-700 lg:w-1/4 md:w-1/2">{props.name}</label>
-      <Toggle
-        onChange={event =>
-          props.dispatch({
-            type: props.name,
-            payload: event.target.checked
-          })
-        }
-        className="w-full mx-2 md:mx-0 self-center"
-        defaultChecked={false}
-      />
+      <span className="w-full">
+        <Toggle
+          onChange={event =>
+            props.dispatch({
+              type: props.name,
+              payload: event.target.checked
+            })
+          }
+          defaultChecked={false}
+        />
+      </span>
       <div className="flex md:flex-col pt-2 md:pt-0 align-center opacity-75">
         <TypeLabel type={props.type} />
         <RequiredLabel required={props.required} />
