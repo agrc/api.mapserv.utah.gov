c293fe7a352a9219ffeb3be642593616bd3b110d
steveoh <sgourley@utah.gov>
2020-04-08 10:28:43 -0600

    feat: purge tailwindcss

--- src/explorer/postcss.config.js
+++ src/explorer/postcss.config.js
@@ -1,7 +1,18 @@
-const tailwindcss = require('tailwindcss');
- module.exports = {
-     plugins: [
-         tailwindcss('./tailwind.js'),
-         require('autoprefixer'),
-     ],
- };
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx'
+  ],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    purgecss
+  ]
+};
