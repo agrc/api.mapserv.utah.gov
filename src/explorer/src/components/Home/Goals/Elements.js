a734d117742c5cf6bfec8029dfab9192dac8eab7
steveoh <sgourley@utah.gov>
2020-04-13 09:57:47 -0600

    style: switch to thumbs up

--- src/explorer/src/components/Home/Goals/Elements.js
+++ src/explorer/src/components/Home/Goals/Elements.js
@@ -4,12 +4,12 @@ export function Item(props) {
   return (
     <div className="flex flex-col">
       <div className="justify-center self-center">
-        <svg className="fill-current text-indigo-600" height="50" width="50" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <defs />
-          <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" fill-rule="evenodd" />
+        <svg className="mb-3 fill-current text-indigo-600" height="30" width="30" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <defs/>
+          <path d="M11 0L9 2v6H2l-2 2v2l2 6 3 2h8l2-2v-8l-3-7V0h-1zm6 10h3v10h-3V10z" fill="current" fill-rule="evenodd"/>
         </svg>
       </div>
-      <div className="uppercase text-center justify-center self-center text-xl font-light">{props.children}</div>
+      <div className="tracking-widest uppercase text-center justify-center self-center text-base font-thin">{props.children}</div>
     </div>
   );
 }
