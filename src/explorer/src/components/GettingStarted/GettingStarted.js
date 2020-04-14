b9f3e51c887d9c0f7fb9bb91d47edaed3d08cac3
steveoh <sgourley@utah.gov>
2020-04-13 19:49:28 -0600

    feat(explorer): getting started guide

--- src/explorer/src/components/GettingStarted/GettingStarted.js
+++ src/explorer/src/components/GettingStarted/GettingStarted.js
@@ -1,155 +1,175 @@
 import React from 'react';
+import CallToAction from '../Action';
+import { Link, H2, P, BrowserKeyExamples, Card, CardItem } from './Elements';
 
 export default function GettingStarted() {
   return (
-    <div className="">
-      <h1>Getting Started Guide</h1>
-      <h2>The What?</h2>
-      <p>
-        The <strong>AGRC Web API</strong> is an <code>http</code> enabeled service for accessing (via the internet) the geospatial data that{' '}
-        <a href="http://gis.utah.gov/about">AGRC</a> stores in the
-        <a href="http://gis.utah.gov/data/how-to-connect-to-the-sgid-via-sde/" title="SGID">
-          State Geographic Information Database (SGID)
-        </a>
-        . These services are a great way to add geospatial functionality to your web pages and applications.
-      </p>
-      <h2>The Who?</h2>
-      <p>This guide and API are designed for people familiar with programming concepts to get started quickly and start making cool apps.</p>
-      <h2>The Why?</h2>
-      <p>
-        The demand for <strong>geospatial</strong> and <strong>location</strong> based information has increased dramatically as many disciplines realize the
-        power of spatial data. A customers address stored in a database has only so many uses. With this API, the door opens to many spatial opportunities from{' '}
-        <strong>visually</strong>
-        seeing customer locations on a map to being able to spatially analyze their relationship or patterns in conjunction with other events such as disease
-        occurrence, natural disaster affected areas, and other location based occurrences.
-      </p>
-      <p>
-        If there is a need to geocode addresses or find out useful information from a physical entity stored in the{' '}
-        <a href="http://gis.utah.gov/data/how-to-connect-to-the-sgid-via-sde/" title="SGID">
-          SGID
-        </a>{' '}
-        using your favorite programming language, then this API for you.
-      </p>
-      <h2>The How?</h2>
-      <p>
-        Make a <code>GET</code> or <code>POST</code> http request to our API and it will reply with the answer. The <a href="/Go/Api">API Explorer</a> lists all
-        the services we currently provide. The explorer also shows you the url pattern for the service, all the required and optional parameters, what http verb
-        to use, as well as a place to make a <strong>sample</strong> request. The exact url for the request will be displayed and the response will also be
-        displayed. The response will be serialized as{' '}
-        <a href="http://json.org" title="JSON">
-          JSON
-        </a>
-        . In order to make a successful request, you will have to generate a unique API Key.
-      </p>
-      <h3>Account Creation</h3>
-      <p>
-        We require you to <a href="/AccountAccess">create an account</a>. Your account gives you access to <a href="/secure/KeyManagement">create and manage</a>{' '}
-        your API keys. It also give you insight to how often your keys are being used. You can judge the popularity of the functionality based on these numbers.
-        Be sure to
-        <a href="/secure/Profile">confirm your email address</a> or API requests will not complete.
-      </p>
-      <h3>Key Creation</h3>
-      <p>
-        Once you have have an account, you can
-        <a href="/secure/GenerateKey">create API keys</a>. Each key is specific to an application you have created; Either a browser or desktop based
-        application.
-      </p>
-      <ul>
-        <li>
-          Browser based applications run in a web browser. eg: <a href="http://atlas.utah.gov">atlas.utah.gov</a>.{' '}
-        </li>
-      </ul>
-      <p>
-        When creating a browser key, you provide the base url for the application. In the example of
-        <code>http://atlas.utah.gov</code>, the url for the key would be <code>atlas.utah.gov/*</code> which is sudo
-        <a href="http://en.wikipedia.org/wiki/Regular_expression">regular expression syntax</a>. All requests that originate from the url provided will
-        authenticate properly and the API will handle your request. Each key you create will track usage and you can look at your analytics in your Console.
-      </p>
+    <article className="text-gray-700">
+      <div className="bg-pattern bg-topo p-6 border-b border-gray-400 shadow-xs">
+        <h1 className="md:text-5xl text-3xl text-gray-800 leading-tight font-light font-extrabold tracking-tight">
+          Getting <span className="text-indigo-600">started</span>
+        </h1>
+        <h2 className="md:text-3xl text-2xl block text-gray-700 font-light tracking-tight">
+          Your first <span className="font-semibold">successful</span> request in <span className="text-indigo-600 font-semibold">3</span> steps.
+        </h2>
+        <section className="px-6 max-w-5xl mx-auto pt-6 ">
+          <div className="mt-10">
+            <ul className="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+              <li>
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white text-3xl font-black">1</div>
+                  </div>
+                  <div className="ml-4">
+                    <h5 className="text-lg leading-6 font-medium">Create a complimentary account</h5>
+                    <p className="mt-2 text-base leading-6 text-gray-500">Manage and view analytics for all of your API keys.</p>
+                  </div>
+                </div>
+              </li>
+              <li className="mt-10 md:mt-0">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white text-3xl font-black">2</div>
+                  </div>
+                  <div className="ml-4">
+                    <h5 className="text-lg leading-6 font-medium">Confirm your email</h5>
+                    <p className="mt-2 text-base leading-6 text-gray-500">Control the bots.</p>
+                  </div>
+                </div>
+              </li>
+              <li className="mt-10 md:mt-0">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white text-3xl font-black">3</div>
+                  </div>
+                  <div className="ml-4">
+                    <h5 className="text-lg leading-6 font-medium">Generate an API key</h5>
+                    <p className="mt-2 text-base leading-6 text-gray-500">Keep track of usage analytics and authenticate requests.</p>
+                  </div>
+                </div>
+              </li>
+              <li className="mt-10 md:mt-0">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white text-3xl font-black">4</div>
+                  </div>
+                  <div className="ml-4">
+                    <h5 className="text-lg leading-6 font-medium">Make a request</h5>
+                    <p className="mt-2 text-base leading-6 text-gray-500">Using your favorite programming language or one of the AGRC tools.</p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <CallToAction></CallToAction>
+      </div>
+      {/* <aside className="px-6 max-w-5xl mx-auto pt-6 mb-10">
+        <div>
+          <H2>Location Matters</H2>
+          <P>
+            The demand for <span className="font-semibold">geospatial</span> and <span className="font-semibold">location</span> based information has increased dramatically as many disciplines realize
+            the power of spatial data. A customer's address stored in a database has only so many uses. With this API, the door opens to many spatial
+            opportunities from <span className="font-semibold">visually</span> seeing customer locations on a map to being able to spatially analyze their relationship or patterns in
+            conjunction with other events such as disease occurrences, natural disaster affected areas, and other location based occurrences.
+          </P>
+        </div>
+      </aside> */}
+      <div className="p-6 border-b border-gray-400 shadow-xs">
+        <section className="px-6 max-w-5xl mx-auto">
+          <H2>Key Creation</H2>
+          <P>
+            Once you have have an account and confirmed ownership of your email address, you can{' '}
+            <a href="https://developer.mapserv.utah.gov/secure/GenerateKey">generate API keys</a>. Each key is specific to an application you have created;
+            Either a browser or desktop based application. Browser based applications run in a web browser. eg: the javascript geocoding component dartboard,
+            running on atlas.utah.gov. The request to the Web API is created in javascript running inside the browser using the browsers fetch API or with an
+            XHR request. Desktop based applications run on a computer. eg: the ArcGIS Pro Geocoding tool running on your desktop. The request to the Web API is
+            coded directly or indirectly from a server side programming or scripting language like Python, Java, or C#.
+          </P>
+        </section>
+      </div>
+      <div className="border-b border-gray-400 shadow-xs">
+        <section className="w-full relative">
+          <div className="bg-indigo-800 h-64 absolute inset-0"></div>
+          <section className="px-6 max-w-5xl mx-auto z-10 relative">
+            <H2 className="pt-3 md:col-span-2 text-white text-center">Choosing the key type</H2>
+            <div className="md:grid md:grid-cols-2 md:col-gap-10">
+              <Card type="Browser" tagLine="Requests are made by JavaScript running in a browser">
+                <CardItem>XHR request</CardItem>
+                <CardItem>window.fetch</CardItem>
+                <CardItem>jquery.ajax</CardItem>
+                <CardItem>esri js api widgets</CardItem>
+                <CardItem>dojo widgets</CardItem>
+                <CardItem>react components</CardItem>
+                <CardItem>angular components</CardItem>
+              </Card>
+              <Card className="mt-6 md:mt-0" type="Desktop" tagLine="Requests are made by code or a tool executing on your computer or a server">
+                <CardItem>ArcPy</CardItem>
+                <CardItem>ArcGIS Desktop</CardItem>
+                <CardItem>Python</CardItem>
+                <CardItem>Java</CardItem>
+                <CardItem>C#</CardItem>
+                <CardItem>AGRC Geocoding Toolbox</CardItem>
+              </Card>
+            </div>
+          </section>
+        </section>
 
-      <table className="table table-condensed">
-        <tbody>
-          <tr>
-            <th>URL Pattern</th>
-            <th>Description</th>
-          </tr>
-          <tr>
-            <td>
-              <code>www.example.com</code>
-              <span className="block">or</span>
-              <span className="block">
-                <code>www.example.com/</code>
+        <section className="max-w-5xl mt-6 md:mx-auto md:grid md:grid-cols-2 md:col-gap-5">
+          <H2 className="px-6 col-span-2">Choosing a browser key</H2>
+          <div class="md:col-span-2 text-center flex mb-6">
+            <div class="p-2 bg-indigo-800 items-center text-indigo-100 lg:rounded-full flex lg:inline-flex" role="alert">
+              <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">tip</span>
+              <span class="font-semibold mr-2 text-left flex-auto">
+                During local development of browser based applications, the Application Status of the key needs to be set to{' '}
+                <span className="font-semibold">Development</span> for requests originating from localhost or machine name urls.
               </span>
-              <span className="block">or</span>
-              <span className="block">
-                <code>www.example.com/*</code>
+            </div>
+          </div>
+          <div className="px-6 max-w-5xl mx-auto">
+            <P>
+              The AGRC Web API collects analytics and authenticates requests with your browser key. Since your application is running in a browser, the API key
+              is in public view. To ensure that the analytics are accurate, the key that you create for the application will contain the url the requests
+              originate from.
+            </P>
+            <P className="mt-6">
+              For example, if your react application is running on atlas.utah.gov, your key pattern will simply equal the DNS name. If your ember application is
+              running on gis.utah.gov/my-application, then your key will also equal the URL.
+            </P>
+            <P className="mt-6">
+              To get the URL pattern correct when creating the key, reference the table for examples. Be sure to always read the response body when making
+              requests to an endpoint. Failed requests will provide helpful information to correct the issue
+            </P>
+          </div>
+          <BrowserKeyExamples className="mx-6 mt-6 md:mt-0 md:max-w-5xl md:mr-6"></BrowserKeyExamples>
+        </section>
+        <section className="max-w-5xl mx-auto mt-6">
+          <H2 className="px-6 col-span-2">Choosing a desktop key</H2>
+          <div class="md:col-span-2 text-center lg:px-4 flex mb-6">
+            <div class="p-2 bg-indigo-800 items-center text-indigo-100 lg:rounded-full flex lg:inline-flex" role="alert">
+              <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">tip</span>
+              <span class="font-semibold mr-2 text-left flex-auto">
+                A common mistake is to use your local IP address. Check with a website like <Link to="https://www.whatismyip.net/">what is my ip</Link> to see
+                what your public IP address is.
               </span>
-            </td>
-            <td>
-              Matches all referrers in the domain <code>www.example.com</code>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code>example.com/*</code>
-            </td>
-            <td>
-              Matches only referrers at <code>example.com</code>, but no subdomains
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code>*.example.com</code>
-            </td>
-            <td>
-              Matches all referrers at all subdomains of <code>example.com</code>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <code>www.examples.com/test</code>
-              <span className="block">or</span>
-              <span className="block">
-                <code>www.example.com/test/</code>
-              </span>
-              <span className="block">or</span>
-              <span className="block">
-                <code>www.example.com/test/*</code>
-              </span>
-            </td>
-            <td>
-              Matches all referrers in <code>www.examples.com/test/</code> and all subpaths
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
-      <p>
-        It is important to note, that when developing on a local computer you create a <code>Development</code> code. localhost or machine name will only work
-        with <code>Development</code> API keys.
-      </p>
-
-      <ul>
-        <li>
-          Desktop based applications run on a computer. eg: <code>your desktop</code>
-        </li>
-      </ul>
-
-      <p>
-        When creating a desktop key, you provide the ip address of that computer. For example, if I was running a python script on my local computer, I would
-        generate a key with my computers ip address - lets call it
-        <code>123.199.0.1</code>.
-      </p>
-
-      <h2>Wrap Up</h2>
-
-      <ol>
-        <li>You've created an account.</li>
-        <li>You've confirmed your email address.</li>
-        <li>You've generated a key.</li>
-        <li>You've chosen what services you want to use.</li>
-      </ol>
-
-      <p>All that is left to do is choose your favorite programming language or framework and start making http requests. </p>
-    </div>
+            </div>
+          </div>
+          <div className="px-6 max-w-5xl mx-auto">
+            <P>
+              The AGRC Web API collects analytics and authenticates requests with your desktop key. To ensure that the analytics are accurate, the key that you
+              create for the application will contain the IP address the requests originate from.
+            </P>
+            <P className="mt-6">
+              For example, if you are using the AGRC Geocoding Tool in ArcGIS Pro, the requests the tool creates will originate from the IP address assigned to
+              you from your internet service provider. This address is not always static and multiple keys may need to be created over time.
+            </P>
+            <P className="mt-6">
+              Be sure to always read the response body when making requests to an endpoint. Failed requests will provide helpful information to correct the
+              issue
+            </P>
+          </div>
+        </section>
+      </div>
+    </article>
   );
 }
