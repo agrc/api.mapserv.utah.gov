7f70e26ca98a0842e19f41f08776a28dbd560517
steveoh <sgourley@utah.gov>
2020-04-16 13:31:10 -0600

    wip(graphql): generate basic api

---
+++ src/graphql/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace graphql
+{
+    public class Program
+    {
+        public static Task Main(string[] args) =>
+            CreateWebHostBuilder(args).Build().RunAsync();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ src/graphql/Query.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using HotChocolate;
+
+namespace graphql {
+    public class Query {
+        public IEnumerable<County> GetCounties() => new List<County> {
+                new County{
+                    Number = "03",
+                    EntityNumber = 2010031010,
+                    EntityYear = 2010,
+                    Name = "CACHE",
+                    Fips = 49005,
+                    StatePlaneRegion = "North",
+                    PopulationLastCensus = 113307,
+                    PopulationCurrentEstimate = 128886
+                }
+            };
+    }
+}
