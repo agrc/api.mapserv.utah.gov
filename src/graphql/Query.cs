213d069cd05afcd0e1f61659430eed77471ff92a
steveoh <sgourley@utah.gov>
2020-04-20 12:02:17 -0600

    refactor(graphql): replace test data

--- src/graphql/Query.cs
+++ src/graphql/Query.cs
@@ -1,19 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using HotChocolate;
+using HotChocolate.Types;
 
 namespace graphql {
     public class Query {
-        public IEnumerable<County> GetCounties() => new List<County> {
-                new County{
-                    Number = "03",
-                    EntityNumber = 2010031010,
-                    EntityYear = 2010,
-                    Name = "CACHE",
-                    Fips = 49005,
-                    StatePlaneRegion = "North",
-                    PopulationLastCensus = 113307,
-                    PopulationCurrentEstimate = 128886
-                }
-            };
+        // [UsePaging]
+        [UseSelection]
+        [UseFiltering]
+        // [UseSorting]
+        public IQueryable<schemas.CountyBoundaries> GetCounties([Service]CloudSqlContext context) => context.CountyBoundaries;
+
+        [UseSelection]
+        [UseFiltering]
+        public IQueryable<schemas.MunicipalBoundaries> GetMunis([Service]CloudSqlContext context) => context.MunicipalBoundaries;
     }
 }
