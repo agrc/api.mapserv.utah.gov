a54174fb0e265d378e1680c58e922a62b8fee819
steveoh <sgourley@utah.gov>
2020-06-17 17:30:42 -0600

    chore(api): configure logging in debug mode

--- WebAPI.Common/Logging/LoggingConfig.cs
+++ WebAPI.Common/Logging/LoggingConfig.cs
@@ -12,8 +12,8 @@ namespace WebAPI.Common.Logging
     {
         public static void Register(string name)
         {
-            var config = new GoogleCloudLoggingSinkOptions {
-                ProjectId = "ut-dts-agrc-web-api-prod",
+            var config = new GoogleCloudLoggingSinkOptions
+            {
                 UseJsonOutput = true,
                 LogName = "api.mapserv.utah.gov",
                 UseSourceContextAsLogName = false,
@@ -22,6 +22,16 @@ namespace WebAPI.Common.Logging
                 ServiceVersion = "1.12.1"
             };
 
+#if DEBUG
+            var projectId = "ut-dts-agrc-web-api-dv";
+            var fileName = "ut-dts-agrc-web-api-dv-log-writer.json";
+            var serviceAccount = File.ReadAllText(Path.Combine(HttpRuntime.AppDomainAppPath, fileName));
+            config.GoogleCredentialJson = serviceAccount;
+#else
+            var projectId = "ut-dts-agrc-web-api-prod";
+#endif
+            config.ProjectId = projectId;
+
             var email = new EmailConnectionInfo
             {
                 EmailSubject = "Geocoding Log Email",
@@ -29,12 +39,10 @@ namespace WebAPI.Common.Logging
                 ToEmail = "SGourley@utah.gov"
             };
 
-            var dir = Path.Combine(HttpRuntime.AppDomainAppPath, $@"..\logs\geocoding\{name}.log-{{Date}}.txt");
             var levelSwitch = new LoggingLevelSwitch();
 
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.ControlledBy(levelSwitch)
-                // .WriteTo.RollingFile(dir)
                 .WriteTo.Email(email, restrictedToMinimumLevel: LogEventLevel.Error)
                 .WriteTo.GoogleCloudLogging(config)
                 .CreateLogger();
