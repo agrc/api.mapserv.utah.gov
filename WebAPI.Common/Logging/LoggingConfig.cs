83719ca1dc7048bcf12a4e95c95725c5b8f962c9
steveoh <sgourley@utah.gov>
2020-06-17 17:32:38 -0600

    release(api): v1.12.2

--- WebAPI.Common/Logging/LoggingConfig.cs
+++ WebAPI.Common/Logging/LoggingConfig.cs
@@ -19,7 +19,7 @@ namespace WebAPI.Common.Logging
                 UseSourceContextAsLogName = false,
                 ResourceType = "global",
                 ServiceName = "api.mapserv.utah.gov",
-                ServiceVersion = "1.12.1"
+                ServiceVersion = "1.12.2"
             };
 
 #if DEBUG
