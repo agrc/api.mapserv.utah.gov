3da2731def15fb2dabbda24c8cee88ab59f68e83
steveoh <sgourley@utah.gov>
2020-07-27 11:32:53 -0600

    test(api): add filter strategy tests

---
+++ test/api.unit.tests/Features/Geocoding/FilterStrategyTests.cs
@@ -0,0 +1,30 @@
+using api.mapserv.utah.gov.Features.Geocoding;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace api.tests.Features.Geocoding {
+    public class FilterStrategyTests {
+        [Fact]
+        public void Should_return_v1_for_api_v1() {
+            var httpContext = Mock.Of<IHttpContextAccessor>(x =>
+                x.HttpContext.Request.RouteValues == new RouteValueDictionary(new { version = "1" }));
+
+            var factory = new FilterSuggestionFactory(httpContext);
+
+            factory.GetStrategy(It.IsAny<int>()).ShouldBeOfType<FilterStrategyV1>();
+        }
+
+        [Fact]
+        public void Should_return_v2_for_api_v2() {
+            var httpContext = Mock.Of<IHttpContextAccessor>(x =>
+                x.HttpContext.Request.RouteValues == new RouteValueDictionary(new { version = "2" }));
+
+            var factory = new FilterSuggestionFactory(httpContext);
+
+            factory.GetStrategy(It.IsAny<int>()).ShouldBeOfType<FilterStrategyV2>();
+        }
+    }
+}
