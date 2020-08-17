5e0758734750f8ea43dd66f354173cb2854dfccd
steveoh <sgourley@utah.gov>
2020-08-17 13:34:12 -0600

    fix(api): fix json.net naming

--- src/api/Features/Milepost/RouteMilepostResponseContract.cs
+++ src/api/Features/Milepost/RouteMilepostResponseContract.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using AGRC.api.Models;
+using Newtonsoft.Json;
 
 namespace AGRC.api.Features.Milepost {
     public class RouteMilepostResponseContract {
@@ -7,20 +8,24 @@ namespace AGRC.api.Features.Milepost {
         /// The datasource for the result
         /// </summary>
         /// <example>SGID10.TRANSPORTATION.UDOTROUTES_LRS</example>
+        [JsonProperty(PropertyName = "source")]
         public string Source { get; set; }
 
         /// <summary>
         /// The geographic coordinates along the route for the given milepost measure
         /// </summary>
+        [JsonProperty(PropertyName = "location")]
         public Point Location { get; set; }
 
         /// <summary>
         /// The route matched
         /// </summary>
         /// <example>Route 0015, Milepost 309.001</example>
+        [JsonProperty(PropertyName = "matchRoute")]
         public string MatchRoute { get; set; }
 
-        [JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        [Newtonsoft.Json.JsonIgnore]
         public string InputRouteMilePost { get; set; }
     }
 }
