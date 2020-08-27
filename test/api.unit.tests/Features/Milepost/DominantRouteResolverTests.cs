6e7a25f59a016505cbd9f4fe1443feb945cf9bff
steveoh <sgourley@utah.gov>
2020-08-27 10:57:14 -0600

    test(api): fix failing test

--- test/api.unit.tests/Features/Milepost/DominantRouteResolverTests.cs
+++ test/api.unit.tests/Features/Milepost/DominantRouteResolverTests.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Formatting;
 using System.Threading;
 using System.Threading.Tasks;
 using AGRC.api.Models.ArcGis;
@@ -30,31 +30,58 @@ namespace AGRC.api.Features.Milepost {
                     }
                 };
 
+                var response = new Concurrencies.ResponseContract {
+                    Locations = new[] {
+                        new Concurrencies.ResponseLocations {
+                            RouteId = "1",
+                            FromMeasure = -1,
+                            ToMeasure = 1,
+                            Concurrencies = new [] {
+                                new Concurrencies.ConcurrencyLocations {
+                                    RouteId = "1",
+                                    FromMeasure = -1,
+                                    ToMeasure = 1,
+                                    IsDominant = true
+                                }
+                            }
+                        },
+                        new Concurrencies.ResponseLocations {
+                            RouteId = "2",
+                            FromMeasure = -1,
+                            ToMeasure = 1,
+                            Concurrencies = new Concurrencies.ConcurrencyLocations[0]
+                        },
+                        new Concurrencies.ResponseLocations {
+                            RouteId = "3",
+                            FromMeasure = -1,
+                            ToMeasure = 1,
+                            Concurrencies = new Concurrencies.ConcurrencyLocations[0]
+                        }
+                    }
+                };
+
                 var computation = new DominantRouteResolver.Computation(locations, null, 0);
 
-                var httpHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-                httpHandler
-                    .Protected()
-                    .Setup<Task<HttpResponseMessage>>("GetAsync",
-                        ItExpr.IsAny<string>(),
-                        ItExpr.IsAny<CancellationToken>())
-                    .ReturnsAsync(new HttpResponseMessage {
-                        StatusCode = HttpStatusCode.OK,
-                        Content = new StringContent(JsonConvert.SerializeObject(new Concurrencies.ResponseContract()))
-                    }).Verifiable();
+                var httpHandler = new Mock<TestingHttpMessageHandler>{ CallBase = true };
 
-                var client = new HttpClient(httpHandler.Object);
+                httpHandler.Setup(x => x.Send(It.IsAny<HttpRequestMessage>())).Returns(new HttpResponseMessage {
+                        StatusCode = HttpStatusCode.OK,
+                        Content = new StringContent(JsonConvert.SerializeObject(response))
+                }).Verifiable();
+
+                var client = new HttpClient(httpHandler.Object) {
+                    BaseAddress = new Uri("https://testing.me")
+                };
 
                 var factory = new Mock<IHttpClientFactory>();
                 factory.Setup(x => x.CreateClient("udot")).Returns(client);
 
                 var log = new Mock<ILogger> { DefaultValue = DefaultValue.Mock };
-
                 var handler = new DominantRouteResolver.Handler(factory.Object, new PythagoreanDistance(), log.Object);
 
                 var result = await handler.Handle(computation, default);
 
-                httpHandler.Protected().Verify("GetAsync", Times.Exactly(3));
+                httpHandler.Verify(x => x.Send(It.IsAny<HttpRequestMessage>()), Times.Exactly(1));
             }
 
             [Fact]
@@ -68,6 +95,15 @@ namespace AGRC.api.Features.Milepost {
             public void Should_order_dominant_then_distance() {
 
             }
+        }
+
+        public class TestingHttpMessageHandler : HttpMessageHandler {
+            public virtual HttpResponseMessage Send(HttpRequestMessage request) =>
+                throw new NotImplementedException("Now we can setup this method with our mocking framework");
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+                CancellationToken cancellationToken) =>
+                    Task.FromResult(Send(request));
         }
     }
 }
