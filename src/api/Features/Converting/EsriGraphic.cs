79f3b41b4ed12382483bda7779c03b5def355af6
steveoh <sgourley@utah.gov>
2020-09-02 18:26:45 -0600

    refactor(api): add esrijson.net and remove json.net

--- src/api/Features/Converting/EsriGraphic.cs
+++ src/api/Features/Converting/EsriGraphic.cs
@@ -1,16 +1,16 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using AGRC.api.Features.Geocoding;
 using AGRC.api.Infrastructure;
 using AGRC.api.Models.ResponseContracts;
 using EsriJson.Net;
-using Newtonsoft.Json.Linq;
 
 namespace AGRC.api.Features.Converting {
     public class EsriGraphic {
-        public class Computation : IComputation<ApiResponseContract<Graphic>> {
+        public class Computation : IComputation<ApiResponseContract<SerializableGraphic>> {
             internal readonly ApiResponseContract<SingleGeocodeResponseContract> Container;
 
             public Computation(ApiResponseContract<SingleGeocodeResponseContract> container) {
@@ -18,8 +18,8 @@ namespace AGRC.api.Features.Converting {
             }
         }
 
-        public class Handler : IComputationHandler<Computation, ApiResponseContract<Graphic>> {
-            public Task<ApiResponseContract<Graphic>> Handle(Computation request, CancellationToken cancellationToken) {
+        public class Handler : IComputationHandler<Computation, ApiResponseContract<SerializableGraphic>> {
+            public Task<ApiResponseContract<SerializableGraphic>> Handle(Computation request, CancellationToken cancellationToken) {
                 EsriJsonObject geometry = null;
                 var attributes = new Dictionary<string, object>();
                 var message = request.Container.Message;
@@ -33,29 +33,43 @@ namespace AGRC.api.Features.Converting {
                         }
                     };
 
-                    attributes = JObject.FromObject(request.Container.Result)
-                                        .ToObject<Dictionary<string, object>>();
+                    attributes = request.Container.Result
+                        .GetType()
+                        .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                        .ToDictionary(key => key.Name, value => value.GetValue(request.Container.Result, null));
                 }
 
                 if (geometry == null && attributes.Count < 1) {
-                    return Task.FromResult(new ApiResponseContract<Graphic> {
+                    return Task.FromResult(new ApiResponseContract<SerializableGraphic> {
                         Status = status,
                         Message = message
                     });
                 }
 
                 var graphic =
-                    new Graphic(geometry,
-                                attributes.Where(x => x.Value != null).ToDictionary(x => x.Key, y => y.Value));
+                    new Graphic(geometry, attributes
+                        .Where(x => x.Value != null)
+                        .ToDictionary(x => x.Key, y => y.Value));
 
-                var responseContainer = new ApiResponseContract<Graphic> {
-                    Result = graphic,
+                var responseContainer = new ApiResponseContract<SerializableGraphic> {
+                    Result = new SerializableGraphic(graphic),
                     Status = status,
                     Message = message
                 };
 
                 return Task.FromResult(responseContainer);
             }
+        }
+
+        public class SerializableGraphic {
+            public SerializableGraphic(Graphic graphic)
+            {
+                Attributes = graphic.Attributes;
+                Geometry = graphic.Geometry;
+            }
+
+            public Dictionary<string, object> Attributes { get; private set; }
+            public object Geometry { get; set; }
         }
     }
 }
