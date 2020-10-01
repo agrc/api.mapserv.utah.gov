156295e8ea197a7ef45946e7310c87e1fb518de9
steveoh <sgourley@utah.gov>
2020-10-01 10:16:01 -0600

    chore(api): remove more roads and highways

---
+++ WebAPI.Domain/ArcServerResponse/Soe/GeocodeMilepostResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI.Domain.ArcServerResponse.Soe
+{
+    public class GeocodeMilepostResponse
+    {
+        public string MatchAddress;
+        public string Geocoder;
+        public double Score;
+        public double UTM_X;
+        public double UTM_Y;
+        public double LONG_X;
+        public double LAT_Y;
+    }
+}
