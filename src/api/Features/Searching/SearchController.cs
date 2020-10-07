46b00ec6bd38195ae1933617ccc1f61c39545c59
steveoh <sgourley@utah.gov>
2020-10-07 13:02:51 -0600

    refactor(api): move search controller logic into query

--- src/api/Features/Searching/SearchController.cs
+++ src/api/Features/Searching/SearchController.cs
@@ -6,7 +6,6 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AGRC.api.Filters;
 using AGRC.api.Infrastructure;
-using AGRC.api.Models.RequestOptionContracts;
 using AGRC.api.Models.ResponseContracts;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -52,83 +51,8 @@ namespace AGRC.api.Features.Searching {
         )]
         [HttpGet]
         [Route("api/v{version:apiVersion}/search/{tableName}/{returnValues}")]
-        public async Task<ObjectResult> Get(string tableName, string returnValues, [FromQuery]SearchRequestOptionsContract options) {
-            _log.Debug("Searching {tableName} for {returnValues} with options: {options}", tableName, returnValues, options);
-
-            #region validation
-
-            var errors = "";
-
-            if (string.IsNullOrEmpty(tableName)) {
-                errors = "tableName is a required field. Input was empty. ";
-            } else if (await _computeMediator.Handle(new ValidateSql.Computation(tableName), default)) {
-                errors += "tableName contains unsafe characters. Don't be a jerk. ";
-            }
-
-            if (string.IsNullOrEmpty(returnValues)) {
-                errors += "returnValues is a required field. Input was empty. ";
-            } else if (await _computeMediator.Handle(new ValidateSql.Computation(returnValues), default)) {
-                errors += "returnValues contains unsafe characters. Don't be a jerk. ";
-            }
-
-            if (options == null) {
-                errors += "Search options did not bind correctly. Sorry. ";
-
-                return BadRequest(new ApiResponseContract<SearchResponseContract> {
-                    Status = (int)HttpStatusCode.BadRequest,
-                    Message = errors
-                });
-            }
-
-            if (!string.IsNullOrEmpty(options.Predicate) &&
-                await _computeMediator.Handle(new ValidateSql.Computation(options.Predicate), default)) {
-                errors += "Predicate contains unsafe characters. Don't be a jerk. ";
-            }
-
-            if (errors.Length > 0) {
-                return BadRequest(new ApiResponseContract<SearchResponseContract> {
-                    Status = (int)HttpStatusCode.BadRequest,
-                    Message = errors
-                });
-            }
-
-            #endregion
-
-            tableName = tableName.ToUpperInvariant();
-            var isStraightSql = !returnValues.ToUpperInvariant().Contains("SHAPE@") &&
-                                string.IsNullOrEmpty(options.Geometry);
-
-            IReadOnlyCollection<SearchResponseContract> result = Array.Empty<SearchResponseContract>();
-
-            try {
-                result = await _mediator.Send(new SqlQuery.Command(tableName, returnValues, options.Predicate, options.AttributeStyle, options.Geometry), default);
-            } catch (Exception ex) {
-                var error = ex.Message.ToUpperInvariant();
-                var message = string.Empty;
-
-                if (error.Contains("INVALID COLUMN NAME")) {
-                    const string pattern = @"\'.*?\'";
-                    var matches = new Regex(pattern).Matches(error);
-
-                    var badColumns = matches.Select(x => x.ToString());
-
-                    message = $"`{tableName}` does not contain an attribute {string.Join(" or ", badColumns)}. Check your spelling.";
-                } else if (error.Contains("AN EXPRESSION OF NON-BOOLEAN TYPE SPECIFIED IN A CONTEXT WHERE A CONDITION IS EXPECTED")) {
-                    message = $"`{options.Predicate}` is not a valid MSSQL where clause.";
-                } else {
-                    message = $"The table `{tableName}` probably does not exist. Check your spelling.";
-                }
-
-                return BadRequest(new ApiResponseContract<SearchResponseContract> {
-                    Status = (int)HttpStatusCode.BadRequest,
-                    Message = message
-                });
-            }
-
-            return Ok(new ApiResponseContract<IReadOnlyCollection<SearchResponseContract>> {
-                Result = result,
-                Status = (int)HttpStatusCode.OK
-            });
-        }
+        public async Task<ObjectResult> Get(
+            string tableName, string returnValues, [FromQuery] SearchRequestOptionsContract options) =>
+                await _mediator.Send(new SearchQuery.Query(tableName, returnValues, options));
     }
 }
