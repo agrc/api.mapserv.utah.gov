c4f512c39bc98fb4c81ce211fb90bee2086afc8d
steveoh <sgourley@utah.gov>
2020-10-07 12:35:47 -0600

    test: remove location from output formatter properties

--- test/api.unit.tests/Features/Converting/GeoJsonTests.cs
+++ test/api.unit.tests/Features/Converting/GeoJsonTests.cs
@@ -45,7 +45,7 @@ namespace api.tests.Features.Converting {
 
             options.Converters.Add(new JsonStringEnumConverter());
 
-            var feature = "{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[1,1]},\"properties\":{\"location\":{\"x\":1,\"y\":1},\"score\":100,\"locator\":\"Centerlines\",\"matchAddress\":\"Matched Address\",\"inputAddress\":\"Input Address\"}}";
+            var feature = "{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[1,1]},\"properties\":{\"score\":100,\"locator\":\"Centerlines\",\"matchAddress\":\"Matched Address\",\"inputAddress\":\"Input Address\",\"srid\":26912}}";
             var resultJson = JsonSerializer.Serialize(result.Result, options);
 
             resultJson.ShouldBe(feature);
