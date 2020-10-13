51b95fbde56a522f721ba674fa66a51950385b49
steveoh <sgourley@utah.gov>
2020-10-13 16:04:54 -0600

    chore(api): replace sql server with postgres

--- src/api/Features/Searching/SearchProviderConfiguration.cs
+++ src/api/Features/Searching/SearchProviderConfiguration.cs
@@ -1,12 +1,12 @@
 ﻿namespace AGRC.api.Features.Searching {
     // TODO Duplicated in developer.mapserv.utah.gov
     public class SearchProviderConfiguration {
-        public string Host { get; set; } = "sgid.agrc.utah.gov";
-        public string Db { get; set; } = "sgid10";
+        public string Host { get; set; } = "opensgid.agrc.utah.gov";
+        public string Db { get; set; } = "opensgid";
         public string Username { get; set; } = "agrc";
         public string Password { get; set; } = "agrc";
 
         public string ConnectionString =>
-            $"Server={Host};User Id={Username};Password={Password};Database={Db}";
+            $"Host={Host};Username={Username};Password={Password};Database={Db};Timeout=5;";
     }
 }
