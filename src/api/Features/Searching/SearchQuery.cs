18290dccfac165dffe171e4da39e70b90b23bba1
steveoh <sgourley@utah.gov>
2020-10-22 15:07:47 -0600

    refactor(api): move validation to behavior

--- src/api/Features/Searching/SearchQuery.cs
+++ src/api/Features/Searching/SearchQuery.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using AGRC.api.Infrastructure;
