26979fc1cb6f4423e4d4f208e9b1888d3cf56e6e
steveoh <sgourley@utah.gov>
2020-11-12 14:06:09 -0700

    chore(api): add scientist and make straight sql query work

--- WebAPI.API/Controllers/API/Version1/SearchController.cs
+++ WebAPI.API/Controllers/API/Version1/SearchController.cs
@@ -8,9 +8,11 @@ using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web.Http;
+using GitHub;
 using Newtonsoft.Json.Linq;
 using Serilog;
 using WebAPI.API.Commands.Search;
+using WebAPI.API.Science;
 using WebAPI.Common.Executors;
 using WebAPI.Common.Extensions;
 using WebAPI.Common.Formatters;
@@ -20,9 +22,11 @@ using WebAPI.Domain.ArcServerInput;
 using WebAPI.Domain.ArcServerResponse.Soe;
 using WebAPI.Domain.InputOptions;
 
-namespace WebAPI.API.Controllers.API.Version1 {
+namespace WebAPI.API.Controllers.API.Version1
+{
 
-    public class SearchController : ApiController {
+    public class SearchController : ApiController
+    {
         [HttpGet]
         public async Task<HttpResponseMessage> Get(string featureClass, string returnValues,
                                                    [FromUri] SearchOptions options)
@@ -60,10 +64,10 @@ namespace WebAPI.API.Controllers.API.Version1 {
 
                 return Request.CreateResponse(HttpStatusCode.InternalServerError,
                                               new ResultContainer<SearchResult>
-                                                  {
-                                                      Status = (int) HttpStatusCode.InternalServerError,
-                                                      Message = errors
-                                                  });
+                                              {
+                                                  Status = (int)HttpStatusCode.InternalServerError,
+                                                  Message = errors
+                                              });
             }
 
             if (!string.IsNullOrEmpty(options.Predicate) &&
@@ -76,88 +80,40 @@ namespace WebAPI.API.Controllers.API.Version1 {
             {
                 return Request.CreateResponse(HttpStatusCode.BadRequest,
                                               new ResultContainer<SearchResult>
-                                                  {
-                                                      Status = (int) HttpStatusCode.BadRequest,
-                                                      Message = errors
-                                                  });
+                                              {
+                                                  Status = (int)HttpStatusCode.BadRequest,
+                                                  Message = errors
+                                              });
             }
 
             #endregion
 
-            // ReSharper disable PossibleNullReferenceException - handled in validation
             featureClass = featureClass.ToUpperInvariant();
 
             var isStraightSql = !returnValues.ToUpperInvariant().Contains("SHAPE@") &&
                                 string.IsNullOrEmpty(options.Geometry);
-            // ReSharper restore PossibleNullReferenceException
 
             if (isStraightSql)
             {
-                specificLog.Warning("search: non spatial query");
-
-                var sqlQueryCommand = new SqlQueryCommand(featureClass, returnValues, options.Predicate);
-                var list = CommandExecutor.ExecuteCommand(sqlQueryCommand);
-
-                if (!string.IsNullOrEmpty(sqlQueryCommand.ErrorMessage))
+               var (message, list) = StraightSqlQuery(featureClass, returnValues, options);
+               if (!string.IsNullOrEmpty(message))
                 {
-                    string message;
-                    var error = sqlQueryCommand.ErrorMessage.ToUpperInvariant();
-
-                    if (error.Contains("INVALID COLUMN NAME"))
-                    {
-                        const string pattern = @"\'.*?\'";
-                        var matches = new Regex(pattern).Matches(sqlQueryCommand.ErrorMessage);
-
-                        var badColumns = new Collection<string>();
-
-                        foreach (var match in matches)
-                        {
-                            badColumns.Add(match.ToString());
-                        }
-
-                        message = "{0} does not contain an attribute {1}. Check your spelling.".With(featureClass,
-                                                                                                     string.Join(
-                                                                                                         " or ",
-                                                                                                         badColumns));
-                    }
-                    else if (error.Contains("AN EXPRESSION OF NON-BOOLEAN TYPE SPECIFIED IN A CONTEXT WHERE A CONDITION IS EXPECTED"))
-                    {
-                        message = "{0} is not a valid ArcObjects where clause.".With(options.Predicate);
-                    }
-                    else
-                    {
-                        message = "{0} probably does not exist. Check your spelling.".With(featureClass);
-                    }
-
-                    specificLog.Warning("search(non-spatial): {message}, {featurClass}, {returnValues}", message, featureClass, returnValues);
-
                     return Request.CreateResponse(HttpStatusCode.BadRequest, new ResultContainer<List<SearchResult>>
-                        {
-                            Message = message,
-                            Status = (int) HttpStatusCode.BadRequest
-                        })
-                                  .AddCache()
-                                  .AddTypeHeader(typeof (ResultContainer<List<SearchResult>>));
-                }
-
-                if (list.Any())
-                {
-                    if (options.AttributeStyle != AttributeStyle.Identical)
                     {
-                        list = CommandExecutor.ExecuteCommand(new FormatAttributesCommand(options.AttributeStyle,
-                                                                                       list));
-                    }
+                        Message = message,
+                        Status = (int)HttpStatusCode.BadRequest
+                    })
+                             .AddCache()
+                             .AddTypeHeader(typeof(ResultContainer<List<SearchResult>>));
                 }
-
-                specificLog.Warning("search(non-spatial): success");
 
                 return Request.CreateResponse(HttpStatusCode.OK, new ResultContainer<List<SearchResult>>
-                    {
-                        Result = list ?? new List<SearchResult>(),
-                        Status = (int) HttpStatusCode.OK
-                    })
-                              .AddCache()
-                              .AddTypeHeader(typeof (ResultContainer<List<SearchResult>>));
+                {
+                    Result = list ?? new List<SearchResult>(),
+                    Status = (int)HttpStatusCode.OK
+                })
+                          .AddCache()
+                          .AddTypeHeader(typeof(ResultContainer<List<SearchResult>>));
             }
 
             var queryArgs = new SpatialQueryArgs(featureClass, returnValues, options);
@@ -177,10 +133,10 @@ namespace WebAPI.API.Controllers.API.Version1 {
 
                 return Request.CreateResponse(HttpStatusCode.InternalServerError,
                                               new ResultContainer<List<SearchResult>>
-                                                  {
-                                                      Status = (int) HttpStatusCode.InternalServerError,
-                                                      Message = "I'm sorry, it seems as though the request had issues."
-                                                  });
+                                              {
+                                                  Status = (int)HttpStatusCode.InternalServerError,
+                                                  Message = "I'm sorry, it seems as though the request had issues."
+                                              });
             }
 
             try
@@ -193,10 +149,10 @@ namespace WebAPI.API.Controllers.API.Version1 {
 
                 return Request.CreateResponse(HttpStatusCode.InternalServerError,
                                               new ResultContainer<List<SearchResult>>
-                                                  {
-                                                      Status = (int) HttpStatusCode.InternalServerError,
-                                                      Message = "I'm sorry, we were unable to communicate with the SOE."
-                                                  });
+                                              {
+                                                  Status = (int)HttpStatusCode.InternalServerError,
+                                                  Message = "I'm sorry, we were unable to communicate with the SOE."
+                                              });
             }
 
             var response = await request.Content.ReadAsAsync<SearchResponse>(new[]
@@ -219,11 +175,11 @@ namespace WebAPI.API.Controllers.API.Version1 {
                 specificLog.Warning("search(spatial): {featureClass} {message} {@options}", featureClass, message, options);
 
                 return Request.CreateResponse(HttpStatusCode.BadRequest, new ResultContainer<List<SearchResult>>
-                    {
-                        Status = (int) HttpStatusCode.BadRequest,
-                        Message = message
-                    })
-                              .AddTypeHeader(typeof (ResultContainer<List<SearchResult>>));
+                {
+                    Status = (int)HttpStatusCode.BadRequest,
+                    Message = message
+                })
+                              .AddTypeHeader(typeof(ResultContainer<List<SearchResult>>));
             }
 
             if (response.Results == null)
@@ -231,19 +187,19 @@ namespace WebAPI.API.Controllers.API.Version1 {
                 specificLog.Warning("search(spatial): success count: 0");
 
                 return Request.CreateResponse(HttpStatusCode.OK, new ResultContainer<List<SearchResult>>
-                    {
-                        Status = (int) HttpStatusCode.OK,
-                        Result = new List<SearchResult>()
-                    })
+                {
+                    Status = (int)HttpStatusCode.OK,
+                    Result = new List<SearchResult>()
+                })
                               .AddCache()
-                              .AddTypeHeader(typeof (ResultContainer<List<SearchResult>>));
+                              .AddTypeHeader(typeof(ResultContainer<List<SearchResult>>));
             }
 
             var resultsWithGeometry = response.Results.Select(x => new SearchResult
-                {
-                    Attributes = x.Attributes,
-                    Geometry = ParseGeometry(x.Geometry)
-                }).ToList();
+            {
+                Attributes = x.Attributes,
+                Geometry = ParseGeometry(x.Geometry)
+            }).ToList();
 
             if (options.AttributeStyle != AttributeStyle.Identical)
             {
@@ -255,18 +211,92 @@ namespace WebAPI.API.Controllers.API.Version1 {
             specificLog.Warning("search(spatial): success count: {count}", resultsWithGeometry.Count());
 
             return Request.CreateResponse(HttpStatusCode.OK, new ResultContainer<List<SearchResult>>
-                {
-                    Status = (int) HttpStatusCode.OK,
-                    Result = resultsWithGeometry
-                })
+            {
+                Status = (int)HttpStatusCode.OK,
+                Result = resultsWithGeometry
+            })
                           .AddCache()
-                          .AddTypeHeader(typeof (ResultContainer<List<SearchResult>>));
+                          .AddTypeHeader(typeof(ResultContainer<List<SearchResult>>));
         }
 
         private static JObject ParseGeometry(string geometry)
         {
             return string.IsNullOrEmpty(geometry) ? null : JObject.Parse(geometry);
         }
+
+        private static (string, List<SearchResult>) StraightSqlQuery(string featureClass, string returnValues, SearchOptions options)
+        {
+            Scientist.ResultPublisher = new ConsolePublisher();
+
+            return Scientist.Science<(string, List<SearchResult>)>("straight-sql-query", experiment =>
+            {
+                experiment.Use(() => MsSqlQuery(featureClass, returnValues, options));
+                experiment.Try(() => OpenSgidQuery(featureClass, returnValues, options));
+            });
+        }
+
+        private static (string, List<SearchResult>) MsSqlQuery(string featureClass, string returnValues, SearchOptions options)
+        {
+            //specificLog.Warning("search: non spatial query");
+
+            var sqlQueryCommand = new SqlQueryCommand(featureClass, returnValues, options.Predicate);
+            var list = CommandExecutor.ExecuteCommand(sqlQueryCommand);
+
+            if (!string.IsNullOrEmpty(sqlQueryCommand.ErrorMessage))
+            {
+                string message;
+                var error = sqlQueryCommand.ErrorMessage.ToUpperInvariant();
+
+                if (error.Contains("INVALID COLUMN NAME"))
+                {
+                    const string pattern = @"\'.*?\'";
+                    var matches = new Regex(pattern).Matches(sqlQueryCommand.ErrorMessage);
+
+                    var badColumns = new Collection<string>();
+
+                    foreach (var match in matches)
+                    {
+                        badColumns.Add(match.ToString());
+                    }
+
+                    message = "{0} does not contain an attribute {1}. Check your spelling.".With(featureClass,
+                                                                                                 string.Join(
+                                                                                                     " or ",
+                                                                                                     badColumns));
+                }
+                else if (error.Contains("AN EXPRESSION OF NON-BOOLEAN TYPE SPECIFIED IN A CONTEXT WHERE A CONDITION IS EXPECTED"))
+                {
+                    message = "{0} is not a valid ArcObjects where clause.".With(options.Predicate);
+                }
+                else
+                {
+                    message = "{0} probably does not exist. Check your spelling.".With(featureClass);
+                }
+
+                //specificLog.Warning("search(non-spatial): {message}, {featurClass}, {returnValues}", message, featureClass, returnValues);
+
+                return (message, (List<SearchResult>)null);
+            }
+
+            if (list.Any())
+            {
+                if (options.AttributeStyle != AttributeStyle.Identical)
+                {
+                    list = CommandExecutor.ExecuteCommand(new FormatAttributesCommand(options.AttributeStyle,
+                                                                                   list));
+                }
+            }
+
+            //specificLog.Warning("search(non-spatial): success");
+
+            return (string.Empty, list);
+        }
+        
+        private static (string, List<SearchResult>) OpenSgidQuery(string featureClass, string returnValues, SearchOptions options)
+        {
+            return (string.Empty, (List<SearchResult>)null);
+        }
     }
+
 
 }
---
+++ WebAPI.API/Science/ConsolePublisher.cs
@@ -0,0 +1,44 @@
+﻿using GitHub;
+using System;
+using Serilog;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Diagnostics;
+
+namespace WebAPI.API.Science
+{
+    public class ConsolePublisher : IResultPublisher
+    {
+        public Task Publish<T, TClean>(Result<T, TClean> result)
+        {
+            Debug.WriteLine($"Publishing results for experiment '{result.ExperimentName}'");
+            Debug.WriteLine($"Result: {(result.Matched ? "MATCH" : "MISMATCH")}");
+            Debug.WriteLine($"Control value: {result.Control.Value}");
+            Debug.WriteLine($"Control duration: {result.Control.Duration}");
+
+            foreach (var observation in result.Candidates)
+            {
+                Debug.WriteLine($"Candidate name: {observation.Name}");
+                Debug.WriteLine($"Candidate value: {observation.Value}");
+                Debug.WriteLine($"Candidate duration: {observation.Duration}");
+            }
+
+            return Task.FromResult(0);
+        }
+    }
+
+    public class LogPublisher : IResultPublisher
+    {
+        public Task Publish<T, TClean>(Result<T, TClean> result)
+        {
+            Log.Debug($"Publishing results for experiment '{result.ExperimentName}'");
+            Log.Debug($"Result: {(result.Matched ? "MATCH" : "MISMATCH")}");
+            Log.Debug($"Control value: {result.Control.Value}");
+            Log.Debug($"Control duration: {result.Control.Duration}");
+
+            return Task.FromResult(0);
+        }
+    }
+
+}
