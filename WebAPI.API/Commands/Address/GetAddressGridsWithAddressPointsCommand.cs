131bc625bb73d39f5c8ce177614887f800214fe5
steveoh <sgourley@utah.gov>
2021-04-13 16:56:54 -0600

    fix(api): replace deprecated sgid10 with open sgid

--- WebAPI.API/Commands/Address/GetAddressGridsWithAddressPointsCommand.cs
+++ WebAPI.API/Commands/Address/GetAddressGridsWithAddressPointsCommand.cs
@@ -1,25 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Configuration;
-using System.Data.SqlClient;
 using Dapper;
+using Npgsql;
 using WebAPI.Common.Abstractions;
 
 namespace WebAPI.API.Commands.Address
 {
     public class GetAddressGridsWithAddressPointsCommand : Command<IEnumerable<string>>
     {
-        public GetAddressGridsWithAddressPointsCommand()
-        {
-            ConnectionString = ConfigurationManager.AppSettings["location_connection"];
-        }
-
-        public GetAddressGridsWithAddressPointsCommand(string connectionString)
-        {
-            ConnectionString = connectionString;
-        }
-
-        public string ConnectionString { get; set; }
-
+        public static readonly string ConnectionString = ConfigurationManager.AppSettings["open_sgid_connection"];
+        private const string Sql = "select distinct addsystem from location.address_points " +
+                                   "order by addsystem asc;";
         public override string ToString()
         {
             return string.Format("{0}, ConnectionString: {1}", "GetAddressGridsWithAddressPointsCommand", ConnectionString);
@@ -27,11 +18,11 @@ namespace WebAPI.API.Commands.Address
 
         protected override void Execute()
         {
-            using (var connection = new SqlConnection(ConnectionString))
-            {
-                connection.Open();
-                Result = connection.Query<string>("SELECT DISTINCT ADDSYSTEM FROM ADDRESSPOINTS");
-            }
+            using var session = new NpgsqlConnection(ConnectionString);
+
+            session.Open();
+
+            Result = session.Query<string>(Sql);
         }
     }
 }
