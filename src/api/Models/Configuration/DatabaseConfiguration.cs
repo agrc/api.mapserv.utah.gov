564556d6a38b324414a0613ec256bb4843b52691
steveoh <sgourley@utah.gov>
2023-03-08 19:53:51 -0700

    chore(api): remove pgsql as a key store

--- src/api/Models/Configuration/DatabaseConfiguration.cs
+++ src/api/Models/Configuration/DatabaseConfiguration.cs
@@ -2,12 +2,6 @@ namespace AGRC.api.Models.Configuration {
     // TODO Duplicated in developer.mapserv.utah.gov
     public class DatabaseConfiguration {
         public string Host { get; set; } = "localhost";
-        public string Port { get; set; } = "5432";
-        public string Db { get; set; } = "webapi";
-        public string Username { get; set; } = "postgres";
-        public string Password { get; set; } = "what password";
-
-        public string ConnectionString =>
-            $"Host={Host};Port={Port};Username={Username};Password={Password};Database={Db};Enlist=false";
+        public string Port { get; set; } = "6379";
     }
 }
