c00f932ca55bf548ffb249974520f67fa6a9b81b
steveoh <sgourley@utah.gov>
2023-03-09 14:29:30 -0700

    refactor(api): switch to to level statement program

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,117 +1,275 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using AGRC.api.Extensions;
+using AGRC.api.Features.Geocoding;
+using AGRC.api.Features.GeometryService;
+using AGRC.api.Features.Health;
+using AGRC.api.Features.Searching;
+using AGRC.api.Infrastructure;
+using api.OpenApi;
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using CorrelationId;
+using CorrelationId.DependencyInjection;
+using MediatR;
+using MediatR.Pipeline;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using NetTopologySuite.IO.Converters;
 using Serilog;
 using Serilog.Sinks.GoogleCloudLogging;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
-namespace AGRC.api {
-    public static class Program {
-        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            // .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
-            .AddEnvironmentVariables()
-            .Build();
+var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-        public static async Task<int> Main(string[] args) {
-            var googleConfig = new GoogleCloudLoggingSinkOptions {
-                LogName = "api.mapserv.utah.gov",
-                UseSourceContextAsLogName = false,
-                ResourceType = "global",
-                ServiceName = "api.mapserv.utah.gov",
-                ServiceVersion = "1.12.2",
-                ProjectId = "ut-dts-agrc-web-api-prod"
-            };
+logger.Information("Starting web host");
 
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+try {
+    var builder = WebApplication.CreateBuilder(args);
 
-            if (new[] { Environments.Development, Environments.Staging }.Contains(environment)) {
-                const string projectId = "ut-dts-agrc-web-api-dev";
-                const string fileName = "ut-dts-agrc-web-api-dev-log-writer.json";
+    builder.Configuration
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables();
 
-                googleConfig.ProjectId = projectId;
+    builder.Logging.ClearProviders();
+    builder.Host
+        .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        .UseSerilog((context, provider) => {
+            provider.ReadFrom.Configuration(context.Configuration);
 
-                try {
-                    googleConfig.GoogleCredentialJson = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), fileName));
-                } catch (FileNotFoundException) {
-                    // use the GOOGLE_APPLICATION_CREDENTIALS
-                }
+            if (context.HostingEnvironment.IsDevelopment()) {
+                provider.WriteTo.Console();
+            } else {
+                provider.WriteTo.GoogleCloudLogging(new GoogleCloudLoggingSinkOptions(
+                    context.Configuration["GoogleCloudLogging:projectId"],
+                    "global",
+                    context.Configuration["GoogleCloudLogging:logName"],
+                    new Dictionary<string, string> {
+                        { "configuration", context.HostingEnvironment.EnvironmentName }
+                    },
+                    null,
+                    false,
+                    true,
+                    null,
+                    context.Configuration["GoogleCloudLogging:serviceName"],
+                    context.Configuration["GoogleCloudLogging:serviceVersion"]
+                ));
             }
+        });
 
-            var logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(Configuration)
-                .WriteTo.GoogleCloudLogging(googleConfig)
-                .CreateLogger();
+    builder.Services.AddDefaultCorrelationId(options => {
+        options.AddToLoggingScope = true;
+        options.EnforceHeader = false;
+        options.IgnoreRequestHeader = true;
+        options.IncludeInResponse = false;
+        options.UpdateTraceIdentifier = false;
+    });
+    builder.Services.AddCors(options => {
+        options.AddDefaultPolicy(builder => builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+    });
+    builder.Services.AddControllers(options => {
+        options.AddApiResponseFormatters();
+        // options.AddJsonpOutputFormatter();
+    })
+    .AddJsonOptions(options => {
+        // open api is currently using system.text.json
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+        options.JsonSerializerOptions.Converters.Add(new GeoJsonConverterFactory());
+    });
 
-            try {
-                logger.Information("Starting web host");
+    builder.Services.AddApiVersioning(x => {
+        x.ReportApiVersions = true;
+        x.AssumeDefaultVersionWhenUnspecified = true;
+        x.DefaultApiVersion = new ApiVersion(1, 0);
+        x.Conventions.Add(new VersionByNamespaceConvention());
+    });
 
-                var host = CreateHostBuilder(args).Build();
+    builder.Services.AddHealthChecks()
+        .AddCheck<StartupHealthCheck>("Startup", failureStatus: HealthStatus.Degraded, tags: new[] { "startup" })
+        .AddCheck<CacheHealthCheck>("Cache", failureStatus: HealthStatus.Degraded, tags: new[] { "health" })
+        .AddCheck<KeyStoreHealthCheck>("KeyStore", failureStatus: HealthStatus.Unhealthy, tags: new[] { "health" })
+        .AddCheck<GeometryServiceHealthCheck>("ArcGIS:GeometryService", failureStatus: HealthStatus.Degraded, tags: new[] { "health" })
+        .AddCheck<UdotServiceHealthCheck>("ArcGIS:RoadsAndHighwaysService", failureStatus: HealthStatus.Degraded, tags: new[] { "health" })
+        .AddCheck<LocatorHealthCheck>("ArcGIS:LocatorServices", tags: new[] { "health" })
+        .AddCheck<BigQueryHealthCheck>("Database", tags: new[] { "health" });
 
-                logger.Information("Completed");
+    builder.Services.UseOptions(builder.Configuration);
+    builder.Services.UseDi(builder.Environment);
 
-                await host.RunAsync();
+    builder.Services.AddSwaggerGen(c => {
+        c.EnableAnnotations();
+        c.DescribeAllParametersInCamelCase();
 
-                return 0;
-            } catch (Exception ex) {
-                logger.Fatal(ex, "Host terminated unexpectedly");
+        c.DocumentFilter<TrimUrlOperationFilter>();
+        c.CustomOperationIds(apiDesc => apiDesc.TryGetMethodInfo(out var methodInfo) ? methodInfo.Name : null);
 
-                return 1;
-            } finally {
-                logger.Information("Shutting down");
-                Log.CloseAndFlush();
+        c.AddSecurityDefinition("apikey", new OpenApiSecurityScheme {
+            Type = SecuritySchemeType.ApiKey,
+            In = ParameterLocation.Query,
+            Name = "apiKey",
+            Description = "A key gathered from developer.mapserv.utah.gov"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+            {
+                new OpenApiSecurityScheme {
+                    Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "apikey" },
+                    Type = SecuritySchemeType.ApiKey,
+                    In = ParameterLocation.Query,
+                    Name = "apiKey",
+                    Description = "A key acquired from developer.mapserv.utah.gov"
+                },
+                Array.Empty<string>()
             }
-        }
+        });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((hostingContext, config) => {
-                if (!hostingContext.HostingEnvironment.IsDevelopment()) {
-                    config.AddJsonFile(
-                        Path.Combine(Path.DirectorySeparatorChar.ToString(), "secrets", "dotnet", "appsettings.Production.json"),
-                        optional: false,
-                        reloadOnChange: true
-                    );
-                }
-            })
-            .ConfigureWebHostDefaults(builder => {
-                builder.UseStartup<Startup>();
-                builder.UseConfiguration(Configuration);
-                builder.ConfigureLogging(x => x.ClearProviders());
-            })
-            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-            .UseSerilog((context, config) => {
-                var googleConfig = new GoogleCloudLoggingSinkOptions {
-                    LogName = "api.mapserv.utah.gov",
-                    UseSourceContextAsLogName = false,
-                    ResourceType = "global",
-                    ServiceName = "api.mapserv.utah.gov",
-                    ServiceVersion = "1.12.2",
-                    ProjectId = "ut-dts-agrc-web-api-prod"
-                };
+        c.AddServer(new OpenApiServer {
+            Url = "https://api.mapserv.utah.gov/api/v1",
+            Description = "Base url for the version 1 API"
+        });
 
-                var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        c.SwaggerDoc("v1", new OpenApiInfo {
+            Version = "v1",
+            Title = "UGRC WebAPI : OpenAPI Documentation",
+            Description = "OpenAPI Documentation",
+            Contact = new OpenApiContact {
+                Name = "UGRC",
+                Email = "sgourley@utah.gov",
+                Url = new Uri("https://github.com/agrc/api.mapserv.utah.gov")
+            },
+            License = new OpenApiLicense {
+                Name = "MIT",
+                Url = new Uri("https://github.com/agrc/api.mapserv.utah.gov/blob/master/LICENSE")
+            },
+        });
 
-                if (new[] { Environments.Development, Environments.Staging }.Contains(environment)) {
-                    const string projectId = "ut-dts-agrc-web-api-dev";
-                    const string fileName = "ut-dts-agrc-web-api-dev-log-writer.json";
-                    googleConfig.ProjectId = projectId;
+        c.SwaggerDoc("v2", new OpenApiInfo {
+            Version = "v2",
+            Title = "UGRC WebAPI : OpenAPI Documentation",
+            Description = "OpenAPI Documentation",
+            Contact = new OpenApiContact {
+                Name = "UGRC",
+                Email = "sgourley@utah.gov",
+                Url = new Uri("https://github.com/agrc/api.mapserv.utah.gov")
+            },
+            License = new OpenApiLicense {
+                Name = "MIT",
+                Url = new Uri("https://github.com/agrc/api.mapserv.utah.gov/blob/master/LICENSE")
+            }
+        });
 
-                    try {
-                        googleConfig.GoogleCredentialJson = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), fileName));
-                    } catch (FileNotFoundException) {
-                        // use the GOOGLE_APPLICATION_CREDENTIALS
-                    }
-                }
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 
-                config.ReadFrom.Configuration(context.Configuration);
-                config.WriteTo.GoogleCloudLogging(googleConfig);
-            });
+        c.IncludeXmlComments(xmlPath);
+    });
+
+    builder.Host.ConfigureContainer<ContainerBuilder>(builder => {
+        builder.RegisterAssemblyTypes(typeof(Program).GetTypeInfo().Assembly)
+                .Where(x => !x.Name.Contains("Decorator"))
+                .AsClosedTypesOf(typeof(IComputationHandler<,>))
+                .AsImplementedInterfaces();
+
+        builder.RegisterDecorator<DoubleAvenuesException.Decorator,
+            IComputationHandler<ZoneParsing.Computation, AddressWithGrids>>();
+
+        builder.RegisterDecorator<SqlQuery.ShapeFieldDecorator,
+            IComputationHandler<SqlQuery.Computation, IReadOnlyCollection<SearchResponseContract>>>();
+
+        builder.RegisterDecorator<SqlQuery.TableMappingDecorator,
+            IComputationHandler<SqlQuery.Computation, IReadOnlyCollection<SearchResponseContract>>>();
+
+        builder.RegisterDecorator<AttributeTableKeyFormatting.Decorator,
+            IComputationHandler<SqlQuery.Computation, IReadOnlyCollection<SearchResponseContract>>>();
+
+        builder.RegisterGenericDecorator(typeof(Reproject.Decorator<,>), typeof(IComputationHandler<,>));
+
+        builder.Register(c => new ComputeMediator(c.Resolve<IComponentContext>().Resolve))
+               .AsImplementedInterfaces()
+               .SingleInstance();
+
+        // set up mediatr
+        builder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly).AsImplementedInterfaces();
+
+        builder
+            .RegisterAssemblyTypes(typeof(Program).GetTypeInfo().Assembly)
+            .AsClosedTypesOf(typeof(IRequestHandler<,>))
+            .AsImplementedInterfaces();
+
+        builder.RegisterGeneric(typeof(RequestPostProcessorBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+        builder.RegisterGeneric(typeof(RequestPreProcessorBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+
+        builder.Register<ServiceFactory>(ctx => {
+            var c = ctx.Resolve<IComponentContext>();
+            return t => c.Resolve(t);
+        });
+    });
+
+    var app = builder.Build();
+
+    app.UseCorrelationId();
+
+    app.UseRouting();
+
+    if (app.Environment.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
     }
+
+    app.UseSwagger(c => c.RouteTemplate = "openapi/{documentName}/api.json");
+
+    app.UseSwaggerUI(c => {
+        c.DocumentTitle = "UGRC WebAPI OpenAPI Documentation";
+        c.RoutePrefix = "openapi";
+        c.SwaggerEndpoint("/openapi/v1/api.json", "v1");
+        c.SwaggerEndpoint("/openapi/v2/api.json", "v2");
+        c.SupportedSubmitMethods();
+        c.EnableDeepLinking();
+        c.DocExpansion(DocExpansion.List);
+    });
+
+    app.MapControllers();
+
+    app.MapHealthChecks("/api/v1/health/details", new HealthCheckOptions {
+        Predicate = healthCheck => healthCheck.Tags.Contains("health"),
+        ResponseWriter = DetailedHealthCheckResponseWriter.WriteDetailsJson
+    });
+
+    app.MapHealthChecks("/api/v1/health", new HealthCheckOptions {
+        Predicate = healthCheck => healthCheck.Tags.Contains("health"),
+    });
+
+    app.MapHealthChecks("", new HealthCheckOptions() {
+        Predicate = healthCheck => healthCheck.Tags.Contains("startup"),
+        ResponseWriter = StartupHealthCheckResponseWriter.WriteText
+    });
+
+    logger.Information("Completed");
+    app.Run();
+} catch (Exception ex) {
+    logger.Fatal(ex, "Host terminated unexpectedly");
+} finally {
+    logger.Information("Shutting down");
+    Log.CloseAndFlush();
 }
