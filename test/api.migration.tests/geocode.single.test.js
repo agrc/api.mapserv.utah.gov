1104216017bafbd040be472242c1e5421fd3f030
steveoh <sgourley@utah.gov>
2023-03-21 11:51:51 -0600

    test(api): new migration equality tests

--- test/api.migration.tests/geocode.single.test.js
+++ test/api.migration.tests/geocode.single.test.js
@@ -7,10 +7,13 @@ const searchParams = {
 const headers = {
   referer: "http://unit.tests",
 };
+const throwHttpErrors = false;
+
 const client = got.extend({
   prefixUrl: "https://api.mapserv.utah.gov/api/v1/geocode/",
   searchParams,
   headers,
+  throwHttpErrors,
 });
 
 // const cloudClient = got.extend({
@@ -22,10 +25,11 @@ const cloudClient = got.extend({
   prefixUrl: "http://localhost:1337/api/v1/geocode/",
   searchParams,
   headers,
+  throwHttpErrors,
 });
 
-describe("single geocoding", () => {
-  it.concurrent("default", async () => {
+describe("geocoding", () => {
+  it.concurrent("single", async () => {
     const url = "326 east south temple/slc";
 
     const agrc = await client.get(url).json();
@@ -33,23 +37,76 @@ describe("single geocoding", () => {
 
     expect(cloud).toEqual(agrc);
   });
-  it.concurrent("esrijson", async () => {
-    const url = "326 east south temple/slc";
-    const searchParams = {
-      format: "esrijson",
-    };
+  it.concurrent("single-404", async () => {
+    const url = "326 east south temple/provo";
 
-    const agrc = await client
-      .get(url, {
-        searchParams,
-      })
-      .json();
-    const cloud = await cloudClient
-      .get(url, {
-        searchParams,
-      })
-      .json();
+    const agrc = await client.get(url).json();
+    const cloud = await cloudClient.get(url).json();
 
     expect(cloud).toEqual(agrc);
+  });
+
+  describe("format", () => {
+    it.concurrent("esrijson", async () => {
+      const url = "326 east south temple/slc";
+      const searchParams = {
+        format: "esrijson",
+      };
+
+      const agrc = await client
+        .get(url, {
+          searchParams,
+        })
+        .json();
+      const cloud = await cloudClient
+        .get(url, {
+          searchParams,
+        })
+        .json();
+
+      expect(cloud).toEqual(agrc);
+    });
+    it.concurrent("esrijson-404", async () => {
+      const url = "326 east south temple/provo";
+      const searchParams = {
+        format: "esrijson",
+      };
+
+      const agrc = await client
+        .get(url, {
+          searchParams,
+        })
+        .json();
+      const cloud = await cloudClient
+        .get(url, {
+          searchParams,
+        })
+        .json();
+
+      expect(cloud).toEqual(agrc);
+    });
+
+    describe("with candidates", () => {
+      it.concurrent("esrijson", async () => {
+        const url = "326 east south temple/slc";
+        const searchParams = {
+          format: "esrijson",
+          suggest: 1,
+        };
+
+        const agrc = await client
+          .get(url, {
+            searchParams,
+          })
+          .json();
+        const cloud = await cloudClient
+          .get(url, {
+            searchParams,
+          })
+          .json();
+
+        expect(cloud).toEqual(agrc);
+      });
+    });
   });
 });
