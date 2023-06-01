3569439ca014257a9f084571c46f4c81ce9b2720
steveoh <sgourley@utah.gov>
2023-06-01 12:35:07 -0600

    chore(api): add empty constructor for serilog

--- src/api/Models/Configuration/DatabaseConfiguration.cs
+++ src/api/Models/Configuration/DatabaseConfiguration.cs
@@ -1,5 +1,8 @@
 namespace AGRC.api.Models.Configuration;
 // TODO Duplicated in developer.mapserv.utah.gov
-public record DatabaseConfiguration(string Host, string Port) {
+public record DatabaseConfiguration {
     public string ConnectionString => $"{Host}:{Port},abortConnect=false";
+
+    public string? Host { get; set; }
+    public int Port { get; set; } = 6379;
 }
