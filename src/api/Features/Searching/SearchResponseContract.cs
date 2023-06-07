ed6e9d2715486d492be5f0c2fc4429e2000b73a5
steveoh <sgourley@utah.gov>
2023-06-07 12:53:20 -0600

    chore(api): update nullability props

--- src/api/Features/Searching/SearchResponseContract.cs
+++ src/api/Features/Searching/SearchResponseContract.cs
@@ -1,6 +1,6 @@
 namespace AGRC.api.Features.Searching;
 public class SearchResponseContract {
-    public object Geometry { get; set; } = default!;
+    public object? Geometry { get; set; }
 
-    public IDictionary<string, object> Attributes { get; set; } = default!;
+    public IDictionary<string, object> Attributes { get; set; } = new Dictionary<string, object>();
 }
