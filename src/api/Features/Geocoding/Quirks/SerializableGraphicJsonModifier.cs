65166c154d7d8899276d171d2c94a0d081c46a6a
steveoh <sgourley@utah.gov>
2023-06-13 13:45:11 -0600

    chore(api): remove log as it's not available anymore

--- src/api/Features/Geocoding/Quirks/SerializableGraphicJsonModifier.cs
+++ src/api/Features/Geocoding/Quirks/SerializableGraphicJsonModifier.cs
@@ -1,15 +1,8 @@
 using System.Text.Json.Serialization.Metadata;
-using Microsoft.Extensions.Logging;
 using static AGRC.api.Features.Converting.EsriGraphic;
-using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace AGRC.api.Quirks;
 public class SerializableGraphicJsonModifier {
-    private readonly ILogger _log;
-
-    public SerializableGraphicJsonModifier(ILogger log) {
-        _log = log;
-    }
     public void ModifyAttributeConverter(JsonTypeInfo typeInfo) {
         if (typeInfo.Type != typeof(SerializableGraphic)) {
             return;
@@ -19,8 +12,6 @@ public class SerializableGraphicJsonModifier {
             if (!propertyInfo.Name.Equals("attributes", StringComparison.OrdinalIgnoreCase)) {
                 continue;
             }
-
-            _log.LogInformation("property info {name}", propertyInfo.Name);
 
             // this property info is the attributes dictionary
             propertyInfo.CustomConverter = new EsriJsonAttributesConverter();
