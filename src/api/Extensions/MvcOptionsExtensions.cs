9d041b376e12b088afeb89ff7336dcd8ecfa03aa
steveoh <sgourley@utah.gov>
2023-06-14 18:02:51 -0600

    chore(api): remove more unused mvc stuff

--- src/api/Extensions/MvcOptionsExtensions.cs
+++
@@ -1,8 +0,0 @@
-using AGRC.api.Filters;
-using Microsoft.AspNetCore.Mvc;
-
-namespace AGRC.api.Extensions;
-public static class MvcOptionsExtensions {
-    public static void AddApiResponseFormatters(this MvcOptions mvcOptions) =>
-        mvcOptions.Filters.Add(typeof(JsonOutputFormatResultFilter));
-}
