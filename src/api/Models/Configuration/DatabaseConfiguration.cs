a30285d89e4942e86cd715eeb6ec3f7df5321bd2
steveoh <sgourley@utah.gov>
2023-06-14 18:04:18 -0600

    chore(api): shorted timeout and initial retry

--- src/api/Models/Configuration/DatabaseConfiguration.cs
+++ src/api/Models/Configuration/DatabaseConfiguration.cs
@@ -1,7 +1,7 @@
 namespace AGRC.api.Models.Configuration;
 // TODO Duplicated in developer.mapserv.utah.gov
 public record DatabaseConfiguration {
-    public string ConnectionString => $"{Host}:{Port},abortConnect=false";
+    public string ConnectionString => $"{Host}:{Port},connectTimeout=200,connectRetry=1,abortConnect=false";
 
     public string? Host { get; set; }
     public int Port { get; set; } = 6379;
