df31c31b34a5b257654e0beec3609bc1b16ef9d0
steveoh <sgourley@utah.gov>
2023-06-20 17:21:29 -0600

    test(api): create tests for new point methods

--- test/api.unit.tests/Models/PointTests.cs
+++ test/api.unit.tests/Models/PointTests.cs
@@ -3,11 +3,33 @@ using AGRC.api.Models;
 namespace api.tests.Models;
 public class PointTests {
     [Fact]
-    public void Should_create_esri_rest_geometry_json() {
-        const string json = """geometries={"geometryType":"esriGeometryPoint","geometries":[{"x":1,"y":0}]}""";
+    public void Should_create_esri_geometry_json() {
+        const string json = """{"x":1,"y":0,"spatialReference":{"wkid":26912}}""";
 
-        var point = new Point(1, 0);
+        var point = new PointWithSpatialReference(1, 0, new(26912, null));
 
-        point.ToQuery().ShouldBe(json);
+        point.ToEsriJson().ShouldBe(json);
+    }
+    [Fact]
+    public void Should_create_pg_geometry() {
+        const string json = """st_pointfromtext('POINT(1 0)',26912)""";
+
+        var point = new PointWithSpatialReference(1, 0, new(26912, null));
+
+        point.ToPostGis().ShouldBe(json);
+    }
+    [Fact]
+    public void Should_create_pg_geometry_with_other_srid() {
+        const string json = """st_transform(st_pointfromtext('POINT(1 0)',3857),26912)""";
+
+        var point = new PointWithSpatialReference(1, 0, new(3857, null));
+
+        point.ToPostGis().ShouldBe(json);
+    }
+    [Fact]
+    public void Should_create_simple_coordinate_pair() {
+        var point = new PointWithSpatialReference(1, 0, new(3857, null));
+
+        point.ToString().ShouldBe("1,0");
     }
 }
