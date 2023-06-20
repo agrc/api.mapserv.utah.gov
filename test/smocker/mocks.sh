bc411d33d67b3d6a7bac193414e4895d9559bf5c
steveoh <sgourley@utah.gov>
2023-06-20 17:21:54 -0600

    test(api): add reverse geocode mocks

--- test/smocker/mocks.sh
+++ test/smocker/mocks.sh
@@ -3,6 +3,7 @@ curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --
 curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@AddressPoints.findAddressCandidates.empty.yml"
 curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@Roads.findAddressCandidates.empty.yml"
 curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@Roads.reverseGeocode.yml"
+curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@Roads.reverseGeocode.4326.yml"
 curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@Roads.healthCheck.yml"
 curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@AddressPoints.healthCheck.yml"
 curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@Geometry.healthCheck.yml"
