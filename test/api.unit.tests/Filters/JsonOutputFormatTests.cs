c201b886a691cb758dca9cbdcb825e3ae48c2001
steveoh <sgourley@utah.gov>
2023-06-21 18:28:55 -0600

    test(api): ignore text for now

--- test/api.unit.tests/Filters/JsonOutputFormatTests.cs
+++ test/api.unit.tests/Filters/JsonOutputFormatTests.cs
@@ -1,156 +1,110 @@
-using AGRC.api.Features.Converting;
-using AGRC.api.Features.Geocoding;
-using AGRC.api.Infrastructure;
-using AGRC.api.Models.ResponseContracts;
-using EsriJson.Net;
-using NetTopologySuite.Features;
-using static AGRC.api.Features.Converting.EsriGraphic;
-using Point = EsriJson.Net.Geometry.Point;
+// using AGRC.api.Features.Geocoding;
+// using AGRC.api.Infrastructure;
+// using AGRC.api.Models.ResponseContracts;
+// using AGRC.api.Quirks;
+// using EsriJson.Net;
+// using NetTopologySuite.Features;
+// using Point = EsriJson.Net.Geometry.Point;
 
-namespace api.tests.Filters;
-public class JsonOutputFormatTests {
-    // [Fact]
-    // public async Task Should_be_ok_if_format_is_unknown() {
-    //     var httpContext = HttpContextHelpers.CreateVersionedHttpContext(1);
-    //     httpContext.Request.QueryString = new QueryString("?format=someUnknownFormat");
+// namespace api.tests.Filters;
+// public class JsonOutputFormatTests {
+//     [Fact]
+//     public async Task Should_be_ok_if_format_is_unknown() {
+//         var httpContext = HttpContextHelpers.CreateVersionedHttpContext(1);
+//         httpContext.Request.QueryString = new QueryString("?format=someUnknownFormat");
 
-    //     var contexts = CreateContext(httpContext);
+//         var filter = new JsonpMiddleware(new Mock<RequestDelegate>().Object);
 
-    //     var httpContextAccessor = new Mock<IHttpContextAccessor>();
-    //     httpContextAccessor.SetupGet(x => x.HttpContext).Returns(httpContext);
+//         await filter.InvokeAsync(httpContext);
+//     }
 
-    //     var mediator = new Mock<IComputeMediator>();
+//     [Fact]
+//     public async Task Should_call_esrijson() {
+//         var httpContext = HttpContextHelpers.CreateVersionedHttpContext(1);
 
-    //     var filter = new JsonOutputFormatResultFilter(mediator.Object);
+//         httpContext.Request.QueryString = new QueryString("?format=esriJSON");
+//         var contexts = HttpContextHelpers.CreateContext(httpContext);
 
-    //     await filter.OnResultExecutionAsync(contexts.ExecutingContext,
-    //                                         () => Task.FromResult(contexts.ExecutedContext));
+//         var httpContextAccessor = new Mock<IHttpContextAccessor>();
+//         httpContextAccessor.SetupGet(x => x.HttpContext).Returns(httpContext);
 
-    //     var result = contexts.ExecutingContext.Result as ObjectResult;
-    //     var result2 = contexts.ExecutedContext.Result as ObjectResult;
+//         var mediator = new Mock<IComputeMediator>();
+//         var response = new ApiResponseContract<SerializableGraphic> {
+//             Result = new SerializableGraphic(new Graphic(new Point(2, 2), new Dictionary<string, object>()))
+//         };
 
-    //     ArgumentNullException.ThrowIfNull(result);
-    //     ArgumentNullException.ThrowIfNull(result2);
+//         mediator.Setup(x => x.Handle(It.IsAny<Computation>(), It.IsAny<CancellationToken>()))
+//                 .Returns(Task.FromResult(response));
 
-    //     result.Value.ShouldBeOfType<ApiResponseContract<SingleGeocodeResponseContract>>();
-    //     result2.Value.ShouldBeOfType<ApiResponseContract<SingleGeocodeResponseContract>>();
-    // }
+//         var filter = new JsonOutputFormatResultFilter(mediator.Object);
 
-    // [Fact]
-    // public async Task Should_call_esrijson() {
-    //     var httpContext = HttpContextHelpers.CreateVersionedHttpContext(1);
+//         await filter.OnResultExecutionAsync(contexts.ExecutingContext, () => Task.FromResult(contexts.ExecutedContext));
 
-    //     httpContext.Request.QueryString = new QueryString("?format=esriJSON");
-    //     var contexts = CreateContext(httpContext);
+//         var result = contexts.ExecutingContext.Result as ObjectResult;
+//         var result2 = contexts.ExecutedContext.Result as ObjectResult;
 
-    //     var httpContextAccessor = new Mock<IHttpContextAccessor>();
-    //     httpContextAccessor.SetupGet(x => x.HttpContext).Returns(httpContext);
+//         ArgumentNullException.ThrowIfNull(result);
+//         ArgumentNullException.ThrowIfNull(result2);
 
-    //     var mediator = new Mock<IComputeMediator>();
-    //     var response = new ApiResponseContract<SerializableGraphic> {
-    //         Result = new SerializableGraphic(new Graphic(new Point(2, 2), new Dictionary<string, object>()))
-    //     };
+//         result.Value.ShouldBeOfType<ApiResponseContract<SerializableGraphic>>();
+//         result2.Value.ShouldBeOfType<ApiResponseContract<SerializableGraphic>>();
+//     }
 
-    //     mediator.Setup(x => x.Handle(It.IsAny<Computation>(), It.IsAny<CancellationToken>()))
-    //             .Returns(Task.FromResult(response));
+//     [Fact]
+//     public async Task Should_call_geojson() {
+//         var httpContext = HttpContextHelpers.CreateVersionedHttpContext(1);
 
-    //     var filter = new JsonOutputFormatResultFilter(mediator.Object);
+//         httpContext.Request.QueryString = new QueryString("?format=GeoJSON");
+//         var contexts = HttpContextHelpers.CreateContext(httpContext);
 
-    //     await filter.OnResultExecutionAsync(contexts.ExecutingContext, () => Task.FromResult(contexts.ExecutedContext));
+//         var httpContextAccessor = new Mock<IHttpContextAccessor>();
+//         httpContextAccessor.SetupGet(x => x.HttpContext).Returns(httpContext);
 
-    //     var result = contexts.ExecutingContext.Result as ObjectResult;
-    //     var result2 = contexts.ExecutedContext.Result as ObjectResult;
+//         var mediator = new Mock<IComputeMediator>();
+//         var response = new ApiResponseContract<Feature> {
+//             Result = new Feature(new NetTopologySuite.Geometries.Point(1, 1), new AttributesTable())
+//         };
 
-    //     ArgumentNullException.ThrowIfNull(result);
-    //     ArgumentNullException.ThrowIfNull(result2);
+//         mediator.Setup(x => x.Handle(It.IsAny<GeoJsonFeature.Computation>(), It.IsAny<CancellationToken>()))
+//                 .Returns(Task.FromResult(response));
 
-    //     result.Value.ShouldBeOfType<ApiResponseContract<SerializableGraphic>>();
-    //     result2.Value.ShouldBeOfType<ApiResponseContract<SerializableGraphic>>();
-    // }
+//         var filter = new JsonOutputFormatResultFilter(mediator.Object);
 
-    // [Fact]
-    // public async Task Should_call_geojson() {
-    //     var httpContext = HttpContextHelpers.CreateVersionedHttpContext(1);
+//         await filter.OnResultExecutionAsync(contexts.ExecutingContext,
+//                                             () => Task.FromResult(contexts.ExecutedContext));
 
-    //     httpContext.Request.QueryString = new QueryString("?format=GeoJSON");
-    //     var contexts = CreateContext(httpContext);
+//         var result = contexts.ExecutingContext.Result as ObjectResult;
+//         var result2 = contexts.ExecutedContext.Result as ObjectResult;
 
-    //     var httpContextAccessor = new Mock<IHttpContextAccessor>();
-    //     httpContextAccessor.SetupGet(x => x.HttpContext).Returns(httpContext);
+//         ArgumentNullException.ThrowIfNull(result);
+//         ArgumentNullException.ThrowIfNull(result2);
 
-    //     var mediator = new Mock<IComputeMediator>();
-    //     var response = new ApiResponseContract<Feature> {
-    //         Result = new Feature(new NetTopologySuite.Geometries.Point(1, 1), new AttributesTable())
-    //     };
+//         result.Value.ShouldBeOfType<ApiResponseContract<Feature>>();
+//         result2.Value.ShouldBeOfType<ApiResponseContract<Feature>>();
+//     }
 
-    //     mediator.Setup(x => x.Handle(It.IsAny<GeoJsonFeature.Computation>(), It.IsAny<CancellationToken>()))
-    //             .Returns(Task.FromResult(response));
+//     [Fact]
+//     public async Task Should_skip_if_format_is_empty() {
+//         var httpContext = HttpContextHelpers.CreateVersionedHttpContext(1);
+//         httpContext.Request.QueryString = new QueryString("?test=1");
 
-    //     var filter = new JsonOutputFormatResultFilter(mediator.Object);
+//         var httpContextAccessor = new Mock<IHttpContextAccessor>();
+//         httpContextAccessor.SetupGet(x => x.HttpContext).Returns(httpContext);
 
-    //     await filter.OnResultExecutionAsync(contexts.ExecutingContext,
-    //                                         () => Task.FromResult(contexts.ExecutedContext));
+//         var mediator = new Mock<IComputeMediator>();
 
-    //     var result = contexts.ExecutingContext.Result as ObjectResult;
-    //     var result2 = contexts.ExecutedContext.Result as ObjectResult;
+//         var filter = new JsonOutputFormatResultFilter(mediator.Object);
 
-    //     ArgumentNullException.ThrowIfNull(result);
-    //     ArgumentNullException.ThrowIfNull(result2);
+//         var contexts = HttpContextHelpers.CreateContext(httpContext);
+//         await filter.OnResultExecutionAsync(contexts.ExecutingContext, () => Task.FromResult(contexts.ExecutedContext));
 
-    //     result.Value.ShouldBeOfType<ApiResponseContract<Feature>>();
-    //     result2.Value.ShouldBeOfType<ApiResponseContract<Feature>>();
-    // }
+//         var result = contexts.ExecutingContext.Result as ObjectResult;
+//         var result2 = contexts.ExecutedContext.Result as ObjectResult;
 
-    // [Fact]
-    // public async Task Should_skip_if_format_is_empty() {
-    //     var httpContext = HttpContextHelpers.CreateVersionedHttpContext(1);
-    //     httpContext.Request.QueryString = new QueryString("?test=1");
+//         ArgumentNullException.ThrowIfNull(result);
+//         ArgumentNullException.ThrowIfNull(result2);
 
-    //     var httpContextAccessor = new Mock<IHttpContextAccessor>();
-    //     httpContextAccessor.SetupGet(x => x.HttpContext).Returns(httpContext);
-
-    //     var mediator = new Mock<IComputeMediator>();
-
-    //     var filter = new JsonOutputFormatResultFilter(mediator.Object);
-
-    //     var contexts = CreateContext(httpContext);
-    //     await filter.OnResultExecutionAsync(contexts.ExecutingContext, () => Task.FromResult(contexts.ExecutedContext));
-
-    //     var result = contexts.ExecutingContext.Result as ObjectResult;
-    //     var result2 = contexts.ExecutedContext.Result as ObjectResult;
-
-    //     ArgumentNullException.ThrowIfNull(result);
-    //     ArgumentNullException.ThrowIfNull(result2);
-
-    //     result.Value.ShouldBeOfType<ApiResponseContract<SingleGeocodeResponseContract>>();
-    //     result2.Value.ShouldBeOfType<ApiResponseContract<SingleGeocodeResponseContract>>();
-    // }
-
-    private static Contexts CreateContext(HttpContext httpContext) {
-        var routeData = new RouteData();
-        var actionDescription = new ActionDescriptor();
-
-        var actionContext = new ActionContext(httpContext, routeData, actionDescription);
-        var filterMetadata = Array.Empty<IFilterMetadata>();
-
-        var actionResult = new ObjectResult(new ApiResponseContract<SingleGeocodeResponseContract>());
-        var controller = new TestController();
-
-        var context = new ResultExecutingContext(actionContext, filterMetadata, actionResult, controller);
-        var resultContext = new ResultExecutedContext(actionContext, filterMetadata, actionResult, controller);
-
-        return new Contexts(context, resultContext);
-    }
-
-    private class Contexts {
-        public Contexts(ResultExecutingContext c1, ResultExecutedContext c2) {
-            ExecutingContext = c1;
-            ExecutedContext = c2;
-        }
-
-        public ResultExecutingContext ExecutingContext { get; }
-        public ResultExecutedContext ExecutedContext { get; }
-    }
-}
-
-internal class TestController : Controller { }
+//         result.Value.ShouldBeOfType<ApiResponseContract<SingleGeocodeResponseContract>>();
+//         result2.Value.ShouldBeOfType<ApiResponseContract<SingleGeocodeResponseContract>>();
+//     }
+// }
