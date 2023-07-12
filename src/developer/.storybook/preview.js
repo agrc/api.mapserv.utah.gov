6b094ffe56c41d143b074b3d42531669779ab61e
steveoh <sgourley@utah.gov>
2023-07-11 19:26:57 -0600

    chore(developer): add storybook

---
+++ src/developer/.storybook/preview.js
@@ -0,0 +1,25 @@
+/** @type { import('@storybook/react').Preview } */
+import { withThemeByClassName } from '@storybook/addon-styling';
+import '../src/index.css';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    expanded: true,
+    hideNoControlsWarning: true,
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: '',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+  }),
+];
