ddeb3fa84183aa3d9d3d2335ec85edbb5d018c35
steveoh <sgourley@utah.gov>
2023-07-13 07:45:57 -0600

    chore(developer): create alternate color link

--- src/developer/src/components/Link.jsx
+++ src/developer/src/components/Link.jsx
@@ -1,11 +1,23 @@
 import PropTypes from 'prop-types';
+import { twMerge } from 'tailwind-merge';
+import { createKeyLookup } from './design-system';
 
-export const TextLink = ({ href, target, children }) => {
+const COLORS = {
+  primary:
+    'decoration-mustard-500 active:text-mustard-500 active:decoration-mustard-600 dark:text-mustard-400/70',
+  secondary:
+    'decoration-mustard-500 dark:active:text-mustard-500 active:decoration-mustard-600 text-mustard-400/70',
+};
+
+export const TextLink = ({ href, target, children, color }) => {
   return (
     <a
       href={href}
       target={target}
-      className="font-medium italic text-mustard-900 underline decoration-mustard-500 underline-offset-2 transition-all hover:decoration-2 hover:underline-offset-4 focus:outline-none active:text-mustard-500 active:decoration-mustard-600 active:underline-offset-8 dark:text-mustard-400/70"
+      className={twMerge(
+        'font-medium italic text-mustard-900 underline underline-offset-2 transition-all hover:decoration-2 hover:underline-offset-4 focus:outline-none active:underline-offset-8',
+        COLORS[color],
+      )}
       rel={target === '_blank' ? 'noopener noreferrer' : undefined}
     >
       {children}
@@ -15,10 +27,13 @@ export const TextLink = ({ href, target, children }) => {
 TextLink.displayName = 'TextLink';
 TextLink.propTypes = {
   href: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(Object.keys(COLORS)),
   target: PropTypes.oneOf(['_blank', '_self', '_parent', '_top']),
   children: PropTypes.node.isRequired,
 };
 TextLink.defaultProps = {
   target: '_self',
+  color: 'primary',
 };
 TextLink.target = ['_blank', '_self', '_parent', '_top'];
+TextLink.Colors = createKeyLookup(COLORS);
