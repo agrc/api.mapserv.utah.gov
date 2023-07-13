310b153f349fc2a32dec0a76995b131e9ff4da4c
steveoh <sgourley@utah.gov>
2023-07-13 07:46:09 -0600

    chore(developer): update pill text color

--- src/developer/src/components/Pill.jsx
+++ src/developer/src/components/Pill.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Pill = ({ children }) => {
   return (
-    <div className="mx-1 my-1 rounded-full border border-slate-400 bg-white px-3 py-1 font-semibold tracking-wide dark:border-slate-800 dark:bg-slate-500 dark:text-slate-50">
+    <div className="mx-1 my-1 rounded-full border border-slate-400 bg-white px-3 py-1 font-semibold tracking-wide text-wavy-800 dark:border-slate-800 dark:bg-slate-500 dark:text-slate-50">
       {children}
     </div>
   );
