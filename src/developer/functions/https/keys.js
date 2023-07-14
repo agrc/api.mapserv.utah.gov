9fedcb67651c7becccd42630cb32afb4426b0589
steveoh <sgourley@utah.gov>
2023-07-13 19:15:09 -0600

    chore(developer): create a module to create api key strings

---
+++ src/developer/functions/https/keys.js
@@ -0,0 +1,21 @@
+import { webcrypto } from 'node:crypto';
+import { v4 as uuid } from 'uuid';
+
+const min = 100000;
+const max = 999999;
+
+/**
+ * A function that creates a new API key
+ * It joins UGRC- with the first 8 characters of a UUID with a random number between 100000 and 999999
+ * @returns {string}
+ */
+export const createKey = () => {
+  const key = uuid();
+  let randomValue;
+
+  do {
+    randomValue = webcrypto.getRandomValues(new Uint32Array(1))[0];
+  } while (randomValue < min || randomValue > max);
+
+  return `UGRC-${key.substring(0, 8).toUpperCase()}${randomValue}`;
+};
