20e4b7226e2edd03c21faff2772a0d29b35e810f
steveoh <sgourley@utah.gov>
2023-07-17 19:25:46 -0600

    refactor(developer): move firestore outside function

--- src/developer/functions/auth/onCreate.js
+++ src/developer/functions/auth/onCreate.js
@@ -1,6 +1,8 @@
 import { getFirestore } from 'firebase-admin/firestore';
 import { error, info } from 'firebase-functions/logger';
 
+const db = getFirestore();
+
 export const createUser = async (user) => {
   info('[auth::user::onCreate] adding user to client connection', user);
 
@@ -10,10 +12,11 @@ export const createUser = async (user) => {
     created: new Date(),
     claimedAccounts: [],
     keys: [],
+    keyIds: [],
   };
 
   try {
-    await getFirestore().collection('clients').doc(user.uid).set(data);
+    await db.collection('clients').doc(user.uid).set(data);
   } catch (errorMessage) {
     error('[auth::user::onCreate] firebase error', { errorMessage, user });
 
