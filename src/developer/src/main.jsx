5cfbb7ac3f5f91776de5f96ad9fab1c104d5ddb2
steveoh <sgourley@utah.gov>
2023-07-25 15:39:44 -0600

    chore(developer): remove utah design system header

--- src/developer/src/main.jsx
+++ src/developer/src/main.jsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import '@utahdts/utah-design-system-header/css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirebaseAppProvider } from 'reactfire';
@@ -38,5 +37,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </FirebaseContainer>
       </FirebaseAppProvider>
     </React.StrictMode>
-  </QueryClientProvider>,
+  </QueryClientProvider>
 );
