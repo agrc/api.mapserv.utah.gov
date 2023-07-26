9abc345fc15cf58ef14af5373a9b8dc904d71cb9
steveoh <sgourley@utah.gov>
2023-07-26 11:53:50 -0600

    style(developer): update menu for dark mode

--- src/developer/src/components/design-system/Menu.jsx
+++ src/developer/src/components/design-system/Menu.jsx
@@ -5,55 +5,14 @@ import PropTypes from 'prop-types';
 import { forwardRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-const MenuLink = ({ to, children }) => {
-  const location = useLocation();
-  const isActive = location.pathname === to;
-
-  return (
-    <NavigationMenu.Link
-      asChild
-      className={clsx(
-        'group relative flex select-none items-center justify-between gap-1 px-3 py-2 font-bold leading-none text-wavy-900 outline-none',
-        {
-          'before:absolute before:-top-1.5 before:left-0 before:z-10 before:block before:h-1 before:w-full before:rounded-full before:bg-mustard-500':
-            isActive,
-        }
-      )}
-    >
-      <Link to={to}>{children}</Link>
-    </NavigationMenu.Link>
+const menuTextCss = (isActive) =>
+  clsx(
+    'group relative flex select-none items-center justify-between gap-1 px-3 py-2 font-bold leading-none text-wavy-900 outline-none dark:text-wavy-100',
+    {
+      'before:absolute before:-top-1.5 before:left-0 before:z-10 before:block before:h-1 before:w-full before:rounded-full before:bg-mustard-500':
+        isActive,
+    }
   );
-};
-MenuLink.propTypes = {
-  to: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
-};
-const MenuTrigger = ({ toArray, children }) => {
-  const location = useLocation();
-  const isActive = toArray.includes(location.pathname);
-
-  return (
-    <NavigationMenu.Trigger
-      className={clsx(
-        'group relative flex select-none items-center justify-between gap-1 px-3 py-2 font-bold leading-none text-wavy-900 outline-none',
-        {
-          'before:absolute before:-top-1.5 before:left-0 before:z-10 before:block before:h-1 before:w-full before:rounded-full before:bg-mustard-500':
-            isActive,
-        }
-      )}
-    >
-      {children}
-      <ChevronDownIcon
-        className="relative h-3 transition-transform duration-300 ease-in group-data-[state=open]:-rotate-180"
-        aria-hidden
-      />
-    </NavigationMenu.Trigger>
-  );
-};
-MenuTrigger.propTypes = {
-  toArray: PropTypes.arrayOf(PropTypes.string).isRequired,
-  children: PropTypes.node.isRequired,
-};
 
 const menuItemCss =
   'absolute left-0 top-0 w-full data-[motion=from-end]:animate-in data-[motion=from-end]:slide-in-from-right data-[motion=from-start]:animate-in data-[motion=from-start]:slide-in-from-left data-[motion=to-end]:animate-out data-[motion=to-end]:slide-out-to-right data-[motion=to-start]:animate-out data-[motion=to-start]:slide-out-to-left sm:w-auto';
@@ -62,10 +21,10 @@ const Menu = () => {
   return (
     <NavigationMenu.Root className="relative z-10 flex w-screen justify-start border-b border-dashed border-b-wavy-300 bg-wavy-200/50">
       <NavigationMenu.List className=" flex list-none justify-center p-1">
-        <NavigationMenu.Item className=" rounded-full hover:bg-wavy-400/50">
+        <NavigationMenu.Item className="rounded-full hover:bg-wavy-400/50">
           <MenuLink to="/self-service">Home</MenuLink>
         </NavigationMenu.Item>
-        <NavigationMenu.Item className=" rounded-full hover:bg-wavy-400/50">
+        <NavigationMenu.Item className="rounded-full hover:bg-wavy-400/50">
           <MenuTrigger
             toArray={['/self-service/create-key', '/self-service/keys']}
           >
@@ -106,10 +65,42 @@ const Menu = () => {
       </NavigationMenu.List>
 
       <div className="absolute left-2 top-full flex w-full justify-start">
-        <NavigationMenu.Viewport className="relative mt-2 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded border bg-white shadow data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out data-[state=open]:zoom-in sm:w-[var(--radix-navigation-menu-viewport-width)]" />
+        <NavigationMenu.Viewport className="relative mt-2 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded border bg-white shadow data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out data-[state=open]:zoom-in dark:border-mustard-500/30 dark:bg-slate-900 sm:w-[var(--radix-navigation-menu-viewport-width)]" />
       </div>
     </NavigationMenu.Root>
   );
+};
+const MenuLink = ({ to, children }) => {
+  const location = useLocation();
+  const isActive = location.pathname === to;
+
+  return (
+    <NavigationMenu.Link asChild className={menuTextCss(isActive)}>
+      <Link to={to}>{children}</Link>
+    </NavigationMenu.Link>
+  );
+};
+MenuLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+const MenuTrigger = ({ toArray, children }) => {
+  const location = useLocation();
+  const isActive = toArray.includes(location.pathname);
+
+  return (
+    <NavigationMenu.Trigger className={menuTextCss(isActive)}>
+      {children}
+      <ChevronDownIcon
+        className="relative h-3 transition-transform duration-300 ease-in group-data-[state=open]:-rotate-180"
+        aria-hidden
+      />
+    </NavigationMenu.Trigger>
+  );
+};
+MenuTrigger.propTypes = {
+  toArray: PropTypes.arrayOf(PropTypes.string).isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 const ListItem = forwardRef(
@@ -118,14 +109,18 @@ const ListItem = forwardRef(
       <NavigationMenu.Link asChild>
         <a
           className={clsx(
-            'block select-none rounded p-3 text-base no-underline outline-none transition-colors hover:bg-slate-100 focus:shadow-[0_0_0_2px] focus:shadow-mustard-400',
+            'block select-none rounded p-3 text-base no-underline outline-none transition-colors hover:bg-slate-100 focus:shadow-[0_0_0_2px] focus:shadow-mustard-400 dark:hover:bg-slate-700/50',
             className
           )}
           {...props}
           ref={forwardedRef}
         >
-          <div className="font-medium text-wavy-700">{title}</div>
-          <p className="text-sm text-wavy-500">{children}</p>
+          <div className="font-medium text-wavy-700 dark:text-mustard-600">
+            {title}
+          </div>
+          <p className="text-sm text-wavy-500 dark:text-mustard-100/50">
+            {children}
+          </p>
         </a>
       </NavigationMenu.Link>
     </li>
