ed344c827a0a309436d3db157a3264571e7b918f
steveoh <sgourley@utah.gov>
2023-08-21 18:02:52 -0600

    chore(developer): convert to batch operation

--- src/developer/migrations/v1.0.2__set-regex.cjs
+++ src/developer/migrations/v1.0.2__set-regex.cjs
@@ -2,35 +2,32 @@ module.exports.migrate = async ({ firestore }) => {
   const generateRegexFromPattern = (
     await import('../functions/https/createKey.js')
   ).generateRegexFromPattern;
-  const clientKeysSnapshot = await firestore.collection('clients').get();
-
-  await clientKeysSnapshot.forEach(async (doc) => {
-    const collection = await firestore
-      .collection(`clients/${doc.id}/keys`)
-      .get();
-
-    collection.forEach(async (doc) => {
-      const key = doc.data();
-      if (key.type !== 'browser') {
-        return;
-      }
-
-      await doc.ref.update({
-        regularExpression: generateRegexFromPattern(key.pattern),
-      });
-    });
-  });
 
   const keysSnapshot = await firestore.collectionGroup('keys').get();
+
+  const batches = [firestore.batch()];
+  let size = 0;
+  let batchIndex = 0;
+
   await keysSnapshot.forEach(async (doc) => {
+    size += 1;
     const key = doc.data();
 
     if (key.type !== 'browser') {
       return;
     }
 
-    await doc.ref.update({
+    batches[batchIndex].update(doc.ref, {
       regularExpression: generateRegexFromPattern(key.pattern),
     });
+
+    if (size % 400 === 0) {
+      batchIndex += 1;
+      batches[batchIndex] = firestore.batch();
+    }
   });
+
+  for (const batch of batches) {
+    await batch.commit();
+  }
 };
