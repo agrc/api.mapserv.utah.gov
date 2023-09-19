7c6197bed53d59743c8ad79417ff3d31123e80c3
steveoh <sgourley@utah.gov>
2023-09-19 17:57:18 -0600

    release(api): v1.13.6

--- WebAPI.API/Properties/AssemblyInfo.cs
+++ WebAPI.API/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("WebAPI.API")]
-[assembly: AssemblyDescription("Web API into UGRC Services")]
+[assembly: AssemblyTitle("UGRC.API")]
+[assembly: AssemblyDescription("The UGRC API for GIS Data and Geocoding Services")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Utah Geospatial Reference Center")]
-[assembly: AssemblyProduct("WebAPI.API")]
-[assembly: AssemblyCopyright("Copyright ©2022")]
+[assembly: AssemblyCompany("Utah Geospatial Resource Center")]
+[assembly: AssemblyProduct("UGRC.API")]
+[assembly: AssemblyCopyright("Copyright ©2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("95a58085-f497-4f79-b1e4-b2422579fb76")]
-[assembly: AssemblyVersion("1.13.5.0")]
-[assembly: AssemblyFileVersion("1.13.5.0")]
+[assembly: AssemblyVersion("1.13.6.0")]
+[assembly: AssemblyFileVersion("1.13.6.0")]
