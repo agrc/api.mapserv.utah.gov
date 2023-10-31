a97311f3479e3d2e6b0a64d33b446e06236fe845
steveoh <sgourley@utah.gov>
2023-10-31 13:36:23 -0600

    fix(developer): migrate database to use flags

---
+++ src/developer/migrations/v1.0.5__refactor-keys-with-flags.cjs
@@ -0,0 +1,42 @@
+module.exports.migrate = async ({ firestore, FieldValue }) => {
+  const keysSnap = await firestore.collectionGroup('keys').get();
+
+  const batches = [firestore.batch()];
+  let size = 0;
+  let batchIndex = 0;
+
+  for (const doc of keysSnap.docs) {
+    size += 1;
+    const data = doc.data();
+
+    batches[batchIndex].set(
+      doc.ref,
+      {
+        flags: {
+          deleted: data.deleted,
+          disabled: data.disabled ?? false,
+          server: data.type === 'server',
+          production: data.mode === 'production',
+        },
+      },
+      { merge: true },
+    );
+
+    batches[batchIndex].update(doc.ref, {
+      status: FieldValue.delete(),
+      deleted: FieldValue.delete(),
+      disabled: FieldValue.delete(),
+      mode: FieldValue.delete(),
+      type: FieldValue.delete(),
+    });
+
+    if (size % 400 === 0) {
+      batchIndex += 1;
+      batches[batchIndex] = firestore.batch();
+    }
+  }
+
+  for (const batch of batches) {
+    await batch.commit();
+  }
+};
