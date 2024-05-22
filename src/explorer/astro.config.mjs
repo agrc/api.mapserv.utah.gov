1aa557581465ec70680bc99dfce5507e21a87c40
stdavis <stdavis@utah.gov>
2024-05-22 16:57:02 -0600

    chore(explorer): initial prettier format

--- src/explorer/astro.config.mjs
+++ src/explorer/astro.config.mjs
@@ -1,68 +1,68 @@
-import starlight from "@astrojs/starlight";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-import starlightLinksValidator from "starlight-links-validator";
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import starlightLinksValidator from 'starlight-links-validator';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlightLinksValidator(),
     starlight({
-      title: "UGRC API Documentation",
+      title: 'UGRC API Documentation',
       // site: "https://ut-dts-agrc-web-api-dev.web.app",
       logo: {
-        src: "./src/assets/api.svg",
+        src: './src/assets/api.svg',
       },
       social: {
-        github: "https://github.com/agrc/api.mapserv.utah.gov",
-        "x.com": "https://x.com/maputah",
-        facebook: "https://www.facebook.com/utahagrc",
-        youtube: "https://youtube.com/@therealugrc",
+        github: 'https://github.com/agrc/api.mapserv.utah.gov',
+        'x.com': 'https://x.com/maputah',
+        facebook: 'https://www.facebook.com/utahagrc',
+        youtube: 'https://youtube.com/@therealugrc',
       },
       sidebar: [
         {
-          label: "Home",
-          link: "/",
+          label: 'Home',
+          link: '/',
         },
         {
-          label: "Reference",
+          label: 'Reference',
           items: [
             {
-              label: "Getting started guide",
-              link: "/getting-started/",
+              label: 'Getting started guide',
+              link: '/getting-started/',
             },
             {
-              label: "Documentation overview",
-              link: "/docs/",
+              label: 'Documentation overview',
+              link: '/docs/',
             },
             {
-              label: "Sample code",
-              link: "https://github.com/agrc/api.mapserv.utah.gov/tree/development/samples",
+              label: 'Sample code',
+              link: 'https://github.com/agrc/api.mapserv.utah.gov/tree/development/samples',
             },
             {
-              label: "Release notes",
-              link: "https://github.com/agrc/api.mapserv.utah.gov/tags",
+              label: 'Release notes',
+              link: 'https://github.com/agrc/api.mapserv.utah.gov/tags',
             },
             {
-              label: "Report a problem",
-              link: "https://github.com/agrc/api.mapserv.utah.gov/issues/new",
+              label: 'Report a problem',
+              link: 'https://github.com/agrc/api.mapserv.utah.gov/issues/new',
             },
             {
-              label: "Privacy policy",
-              link: "/privacy/",
+              label: 'Privacy policy',
+              link: '/privacy/',
             },
           ],
         },
         {
-          label: "Endpoints (version 1)",
+          label: 'Endpoints (version 1)',
           autogenerate: {
-            directory: "/docs/v1/endpoints/",
+            directory: '/docs/v1/endpoints/',
           },
         },
       ],
-      customCss: ["./src/tailwind.css"],
+      customCss: ['./src/tailwind.css'],
     }),
     tailwind({
       applyBaseStyles: true,
