802ee706bf6c74a2ff768e7c69fb90b4e099d1d4
steveoh <sgourley@utah.gov>
2024-08-12 18:50:31 -0600

    refactor(api): improve speed of TopNList datastructure

--- src/api/Services/DataStructures/TopNList.cs
+++ src/api/Services/DataStructures/TopNList.cs
@@ -4,16 +4,22 @@ namespace ugrc.api.Services;
 /// </summary>
 /// <typeparam name="T"></typeparam>
 public abstract class TopNList<T>(int nItems, IComparer<T> comparer) {
-    private readonly SortedSet<T> _items = new SortedSet<T>(comparer);
+    private readonly SortedSet<T> _items = new(comparer);
 
     private int Size { get; } = nItems;
 
     public virtual void Add(T candidate) {
-        _items.Add(candidate);
-        if (_items.Count > Size) {
-            _items.Remove(_items.Last());
+        if (_items.Count < Size) {
+            _items.Add(candidate);
+        } else if (comparer.Compare(candidate, _items.Max) < 0) {
+            var max = _items.Max;
+
+            if (max is not null) {
+                _items.Remove(max);
+            }
+            _items.Add(candidate);
         }
     }
 
-    public virtual IList<T> Get() => _items.ToList();
+    public virtual IList<T> Get() => [.. _items];
 }
