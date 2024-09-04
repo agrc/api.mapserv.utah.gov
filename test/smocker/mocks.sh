b8502b3375d0161e6acc6119c6bb1227ac1fc592
steveoh <sgourley@utah.gov>
2024-09-04 15:24:03 -0600

    test(api): add smocks for geometry service

--- test/smocker/mocks.sh
+++ test/smocker/mocks.sh
@@ -6,3 +6,5 @@ curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --
 curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@Roads.reverseGeocode.4326.yml"
 curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@Roads.healthCheck.yml"
 curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@AddressPoints.healthCheck.yml"
+curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@GeometryService.project.yml"
+curl -X POST localhost:8081/mocks --header "Content-Type: application/x-yaml" --data-binary "@GeometryService.healthCheck.yml"
