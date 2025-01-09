640c55a9033d119b6fd94040d960d7932dd098cf
steveoh <sgourley@utah.gov>
2025-01-08 19:50:49 -0700

    refactor(developer): swap to utah design system
    and react-router

--- src/developer/src/App.jsx
+++ src/developer/src/App.jsx
@@ -1,6 +1,4 @@
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getFunctions } from 'firebase/functions';
+import { useFirebaseAuth } from '@ugrc/utah-design-system';
 import { Suspense } from 'react';
 import {
   Outlet,
@@ -9,74 +7,57 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   redirect,
-} from 'react-router-dom';
-import { useFirebaseApp, useSigninCheck } from 'reactfire';
-import connectEmulators from './Emulators';
+} from 'react-router';
 import Layout from './components/page/Layout';
 
 const App = () => {
-  const app = useFirebaseApp();
-  const firestore = getFirestore(app);
-  const functions = getFunctions(app);
-  const auth = getAuth(app);
-  const { status, data: signInCheck } = useSigninCheck();
-  const { data: user } = useSigninCheck();
+  const { currentUser } = useFirebaseAuth();
 
-  connectEmulators(import.meta.env.DEV, auth, firestore, functions);
+  const anonymous = !currentUser ?? false;
 
-  const anonymous = !(signInCheck?.signedIn ?? false);
-
-  let router = {};
-  if (status !== 'loading') {
-    router = createBrowserRouter(
-      createRoutesFromElements(
-        <Route element={<Layout />}>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<Layout />}>
+        <Route
+          index
+          loader={() => promotableRoute(anonymous)}
+          lazy={() => import('./components/page/Landing')}
+        />
+        <Route path="/self-service" Component={Outlet}>
           <Route
             index
-            loader={() => promotableRoute(anonymous)}
-            lazy={() => import('./components/page/Landing')}
+            loader={() => protectedRoute(anonymous, currentUser)}
+            lazy={() => import('./components/page/SelfService')}
           />
-          <Route path="/self-service" Component={Outlet}>
-            <Route
-              index
-              loader={() => protectedRoute(anonymous, user)}
-              lazy={() => import('./components/page/SelfService')}
-            />
-            <Route
-              path="create-key"
-              loader={() => protectedRoute(anonymous, user)}
-              lazy={() => import('./components/page/CreateKey')}
-            />
-            <Route
-              path="keys"
-              loader={() => protectedRoute(anonymous, user)}
-              lazy={() => import('./components/page/Keys')}
-            />
-            <Route
-              path="keys/:key"
-              loader={() => protectedRoute(anonymous, user)}
-              lazy={() => import('./components/page/Key')}
-            />
-            <Route
-              path="claim-account"
-              loader={() => protectedRoute(anonymous, user)}
-              lazy={() => import('./components/page/ClaimAccount')}
-            />
-          </Route>
-          <Route path="*" loader={() => redirect('/')} />
-        </Route>,
-      ),
-      {
-        future: {
-          v7_normalizeFormMethod: true,
-        },
-      },
-    );
-  }
+          <Route
+            path="create-key"
+            loader={() => protectedRoute(anonymous, currentUser)}
+            lazy={() => import('./components/page/CreateKey')}
+          />
+          <Route
+            path="keys"
+            loader={() => protectedRoute(anonymous, currentUser)}
+            lazy={() => import('./components/page/Keys')}
+          />
+          <Route
+            path="keys/:key"
+            loader={() => protectedRoute(anonymous, currentUser)}
+            lazy={() => import('./components/page/Key')}
+          />
+          <Route
+            path="claim-account"
+            loader={() => protectedRoute(anonymous, currentUser)}
+            lazy={() => import('./components/page/ClaimAccount')}
+          />
+        </Route>
+        <Route path="*" loader={() => redirect('/')} />
+      </Route>,
+    ),
+  );
 
   return (
     <Suspense fallback={<div> loading...</div>}>
-      {status === 'loading' ? null : <RouterProvider router={router} />}
+      <RouterProvider router={router} />
     </Suspense>
   );
 };
