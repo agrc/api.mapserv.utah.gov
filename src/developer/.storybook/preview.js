c1bd530b323739e3ad4f774e2d5f559aa51586b0
steveoh <sgourley@utah.gov>
2025-07-30 11:19:35 -0600

    deps: upgrade storybook and @ugrc packages

--- src/developer/.storybook/preview.js
+++ src/developer/.storybook/preview.js
@@ -1,4 +1,4 @@
-/** @type { import('@storybook/react').Preview } */
+/** @type { import('@storybook/react-vite').Preview } */
 import { withThemeByClassName } from '@storybook/addon-themes';
 import '../src/index.css';
 
