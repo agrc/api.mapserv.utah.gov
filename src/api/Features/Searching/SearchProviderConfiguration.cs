06a62b8b142e5127a3e960ce3a37dd35a6264fff
steveoh <sgourley@utah.gov>
2026-01-29 21:53:20 -0700

    chore(api): use new open sgid dns

--- src/api/Features/Searching/SearchProviderConfiguration.cs
+++ src/api/Features/Searching/SearchProviderConfiguration.cs
@@ -1,6 +1,7 @@
 namespace ugrc.api.Features.Searching;
+
 public class SearchProviderConfiguration {
-    public string Host { get; set; } = "opensgid.agrc.utah.gov";
+    public string Host { get; set; } = "opensgid.ugrc.utah.gov";
     public string Db { get; set; } = "opensgid";
     public string Username { get; set; } = "agrc";
     public string Password { get; set; } = "agrc";
