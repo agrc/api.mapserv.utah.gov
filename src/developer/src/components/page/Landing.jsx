7de6266728e03caaab554799706a7b903f75ef49
stdavis <stdavis@utah.gov>
2026-09-08 09:12:35 -0600

    deps(developer): tailwind v3 -> v4

--- src/developer/src/components/page/Landing.jsx
+++ src/developer/src/components/page/Landing.jsx
@@ -1,12 +1,14 @@
-import { Banner, ExternalLink, UtahIdLogin } from '@ugrc/utah-design-system';
+import { Banner } from '@ugrc/utah-design-system/components/Banner';
+import { ExternalLink } from '@ugrc/utah-design-system/components/Link';
+import { UtahIdLogin } from '@ugrc/utah-design-system/components/UtahIdLogin';
 import product from './product.png';
 
 export function Component() {
   return (
     <section className="mx-auto max-w-full px-3 lg:px-0">
       <div className="flex flex-wrap">
-        <div className="lg:flex-basis-auto min-h-[480px] w-full max-w-full py-10 lg:min-h-[980px] lg:w-1/2 lg:shrink-0 lg:grow-0 lg:py-[114px]">
-          <div className="mx-auto grid w-full max-w-[480px] gap-6">
+        <div className="lg:flex-basis-auto min-h-120 w-full max-w-full py-10 lg:min-h-245 lg:w-1/2 lg:shrink-0 lg:grow-0 lg:py-28.5">
+          <div className="mx-auto grid w-full max-w-120 gap-6">
             <h2 className="text-primary-800 dark:text-slate-100">Access your keys</h2>
             <p className="text-primary-900 dark:text-slate-100">
               The UGRC API requires a UtahID account to create and manage API keys. Your name and email address will be
@@ -35,8 +37,8 @@ export function Component() {
             />
           </div>
         </div>
-        <div className="lg:flex-basis-auto relative hidden flex-col items-center justify-center bg-gradient-to-b from-primary-700 via-primary-400 to-secondary-400 lg:block lg:w-1/2 lg:shrink-0 lg:grow-0">
-          <img className="absolute left-0 top-0 h-full w-full" src="/sparkles.png" loading="lazy" alt=""></img>
+        <div className="lg:flex-basis-auto from-primary-700 via-primary-400 to-secondary-400 relative hidden flex-col items-center justify-center bg-gradient-to-b lg:block lg:w-1/2 lg:shrink-0 lg:grow-0">
+          <img className="absolute top-0 left-0 h-full w-full" src="/sparkles.png" loading="lazy" alt=""></img>
           <h2 className="mt-12 w-full text-center text-white">Location Matters</h2>
           <h3 className="w-full text-center text-white">Let data take you places</h3>
           <div>
