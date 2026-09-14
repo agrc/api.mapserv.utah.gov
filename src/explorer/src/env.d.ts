5382c79c407b6e8873ccee7328a5affefd33a8ab
stdavis <stdavis@utah.gov>
2026-09-14 15:12:32 -0600

    fix(explorer): switch to official DTS header and footer

--- src/explorer/src/env.d.ts
+++ src/explorer/src/env.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="../.astro/env.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module '@utahdts/utah-design-system-header/css';
